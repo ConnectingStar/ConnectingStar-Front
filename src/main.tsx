916f694c61472ec0a8f742fce83f4de0ca929ff5
sikkzz <sling0623@gmail.com>
2024-01-19 19:19:26 +0900

    [TS-176] Refactor: directory name change

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import { Global } from "@emotion/react";
 
 import App from "@/App";
-import { GlobalStyle } from "@/styles/GlobalStyle";
+import { GlobalStyle } from "@/change/GlobalStyle";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
