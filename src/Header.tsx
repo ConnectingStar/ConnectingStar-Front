1aa5cb85d733d0b76b64ab628af3749c5612f5d9
khkh0109 <ist200120@gmail.com>
2024-01-30 21:06:37 +0900

    [TS-228] Feat: Header 컴포넌트 추가
    - Header와 관련된 컴포넌트 추가함(Header, PrevButton, CloseButton, Title, ActionButton)

---
+++ src/Header.tsx
@@ -0,0 +1,61 @@
+import { ComponentPropsWithoutRef } from "react";
+
+import ArrowLeftIcon from "./arrow-left.svg?react";
+import CloseIcon from "./close.svg?react";
+
+import { headerStyle } from "./Header.style";
+
+export default function Header({ children, ...props }: ComponentPropsWithoutRef<"div">) {
+	return (
+		<div css={headerStyle.container} {...props}>
+			{children}
+		</div>
+	);
+}
+
+Header.PrevButton = function HeaderPrevButton() {
+	return (
+		<button
+			type="button"
+			css={headerStyle.leftIconButton}
+			onClick={(e) => console.log("prev icon click: ", e)}
+		>
+			<ArrowLeftIcon />
+		</button>
+	);
+};
+
+// NOTE: 닫기 버튼 기능 구현은 어떻게 할지..
+Header.CloseButton = function HeaderCloseButton() {
+	return (
+		<button
+			type="button"
+			css={headerStyle.leftIconButton}
+			onClick={(e) => console.log("close icon click: ", e)}
+		>
+			<CloseIcon />
+		</button>
+	);
+};
+
+// NOTE: 이 부분 props 이름을 어떻게 지어야 할지 모르겠음. size는 명확하지 않은듯
+interface HeaderTitleProps {
+	size?: "default" | "large";
+	children: React.ReactNode;
+}
+
+Header.Title = function HeaderTitle({ size = "default", children }: HeaderTitleProps) {
+	return <h1 css={size === "large" ? headerStyle.titleLarge : headerStyle.title}>{children}</h1>;
+};
+
+// NOTE: ActionButton이라는 이름보다 더 괜찮은 이름 없는지
+Header.ActionButton = function HeaderActionButton({
+	children,
+	...props
+}: ComponentPropsWithoutRef<"button">) {
+	return (
+		<button css={headerStyle.actionButton} {...props}>
+			{children}
+		</button>
+	);
+};
