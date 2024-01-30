afc929cbcd5f0289ebfbe446f7b7ab1b156bc825
pizzaYami <cyj9407@naver.com>
2024-01-30 13:31:57 +0900

    [TS-206] Style: theme color변경

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,5 @@
 export const theme = {
-	fonts: {
+	size: {
 		HEAD_A: {
 			fontSize: "1.5rem",
 			fontWeight: "500",
@@ -53,39 +53,28 @@ export const theme = {
 			fontWeight: "500",
 		},
 	},
-	colors: {
+	color: {
 		// 폰트 색상
-		FONT_COLOR_BLACK: { color: "rgba(17, 17, 17, 1)" },
-		FONT_COLOR_DEEP_GRAY: { color: "rgba(80, 80, 80, 1)" },
-		FONT_COLOR_GRAY: { color: "rgba(118, 118, 118, 1)" },
+		font_black: "#111111",
+		font_deepGray: "#505050",
+		font_gray: "#767676",
+
 		// 색상
-		MAIN_DEEP_BLUE: { color: "rgba(56, 86, 120, 1)" },
-		MAIN_DEEP_BLUE_BG: { backgroundColor: "rgba(56, 86, 120, 1)" },
-
-		MAIN_LIGHT_BLUE: { color: "rgba(1, 118, 249, 1)" },
-		MAIN_LIGHT_BLUE_BG: { backgroundColor: "rgba(1, 118, 249, 1)" },
-
-		MAIN_PALE_BLUE: { color: "rgba(1, 118, 249, 0.1)" },
-		MAIN_PALE_BLUE_BG: { backgroundColor: "rgba(1, 118, 249, 0.1)" },
-
-		MAIN_LIGHT_YELLOW: { color: "rgba(255, 226, 118, 1)" },
-		MAIN_LIGHT_YELLOW_BG: { backgroundColor: "rgba(255, 226, 118, 1)" },
+		main_deepBlue: "#385678",
+		main_Blue: "#0176F9",
+		main_lightBlue: "#E6F1FE",
+		main_yellow: "#FFE276",
 
 		// 줄, 배경
-		MAIN_LINE: { color: "rgba(240, 240, 246, 1)" },
-		MAIN_BG: { backgroundColor: "rgba(247, 247, 251, 1)" },
+		line: "#F0F0F6",
+		BG: "#F7F7FB",
 
 		// 버튼
-		BUTTON_DISABLED: { color: "rgba(229, 229, 236, 1)" },
-		BUTTON_DISABLED_BG: { backgroundColor: "rgba(229, 229, 236, 1)" },
+		button_disabled: "#E5E5EC",
+		button_deactivated: "#999999",
 
-		BUTTON_DEACTIVATED: { color: "rgba(153, 153, 153, 1)" },
-		BUTTON_DEACTIVATED_BG: { backgroundColor: "rgba(153, 153, 153, 1)" },
 		// 시스템 색상
-		SYSTEM_SUCCESS: { color: "rgba(4, 176, 20, 1)" },
-		SYSTEM_SUCCESS_BG: { backgroundColor: "rgba(4, 176, 20, 1)" },
-
-		SYSTEM_FAILURE: { color: "rgba(220, 0, 0, 1)" },
-		SYSTEM_FAILURE_BG: { backgroundColor: "rgba(220, 0, 0, 1)" },
+		success: "#04B014",
+		error: "#DC0000",
 	},
 };
