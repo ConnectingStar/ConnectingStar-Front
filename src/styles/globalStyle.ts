aa6ce0eb47fb9ba692dd46f8f41b3b91497365d8
pizzaYami <cyj9407@naver.com>
2024-01-30 16:17:13 +0900

    [TS-206] Fix : global font-family root에 넣도록 변경

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -9,8 +9,7 @@ export const GlobalStyle = css`
 	* {
 		margin: 0; /* margin 초기화 */
 		padding: 0; /* padding 초기화 */
-		font-family: "Noto Sans KR", sans-serif;
-		// font: inherit; /* font 상속을 못하는 요소(button, input)에게 폰트 적용을 위해 작성 */
+		font: inherit; /* font 상속을 못하는 요소(button, input)에게 폰트 적용을 위해 작성 */
 		color: inherit; /* a, input, textarea에도 동일한 글자색 적용을 위해 작성 */
 	}
 
@@ -21,6 +20,7 @@ export const GlobalStyle = css`
 	}
 
 	:root {
+		font-family: "Noto Sans KR", sans-serif;
 		-webkit-text-size-adjust: 100%; /* 모바일에서도 원래 폰트 사이즈로 출력(사파리) */
 		text-size-adjust: 100%; /* 모바일에서도 원래 폰트 사이즈로 출력 */
 		overflow-wrap: break-word;
