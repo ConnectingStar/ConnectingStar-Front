e2b443bcff22e757e05b55914571ad9688208a43
khkh0109 <ist200120@gmail.com>
2024-02-01 21:41:10 +0900

    [TS-228] Refactor: 아이콘 파일 assets/icon 폴더로 이동 및 import 경로변경
    - assets/icon 폴더 내부에 있는 .gitkeep 파일 삭제
    - arrow-left.svg, close.svg 아이콘 파일 assets/icon 폴더로 이동
    - Header.tsx에 있는 아이콘 import 경로 변경

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,7 +1,7 @@
 import { ComponentPropsWithoutRef, ReactNode } from "react";
 
-import ArrowLeftIcon from "@/arrow-left.svg?react";
-import CloseIcon from "@/close.svg?react";
+import ArrowLeftIcon from "@/assets/icon/arrow-left.svg?react";
+import CloseIcon from "@/assets/icon/close.svg?react";
 import { headerStyle } from "@/Header.style";
 
 // NOTE: 이 부분 props 이름을 어떻게 지어야 할지 모르겠음. size는 명확하지 않은듯
