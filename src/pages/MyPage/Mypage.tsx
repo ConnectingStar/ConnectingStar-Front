4a1304ad31f75c8a399cd1eaeb3d37604e8024c1
sikkzz <sling0623@gmail.com>
2024-02-05 18:03:50 +0900

    Design: mypage landing styling

---
+++ src/pages/MyPage/Mypage.tsx
@@ -0,0 +1,57 @@
+import RightArrowIcon from "@/assets/icon/ic-right-arrow.svg?react";
+import Button from "@/components/MyPage/Button";
+import { buttonData, buttonDataWithIcon } from "@/constants/myPageConstants";
+
+import {
+	layoutStyle,
+	headingStyle,
+	profileBoxStyle,
+	profileImgStyle,
+	buttonBoxStyle,
+} from "@/pages/MyPage/Mypage.style";
+
+const Mypage = () => {
+	return (
+		<div css={layoutStyle}>
+			<h2 css={headingStyle}>마이 페이지</h2>
+			<div css={profileBoxStyle}>
+				<div css={profileImgStyle} />
+				<a href="#">
+					<p>사용자 닉네임</p>
+					<RightArrowIcon />
+				</a>
+			</div>
+
+			{buttonDataWithIcon.map((data) => (
+				<div css={buttonBoxStyle} key={data.id}>
+					{data.id === "프로필" ? "" : <h3>{data.id}</h3>}
+					{data.button.map((buttonData) => (
+						<Button
+							key={buttonData.text}
+							icon={buttonData.icon}
+							text={buttonData.text}
+							arrowIcon={false}
+							isVersion={false}
+						/>
+					))}
+				</div>
+			))}
+
+			{buttonData.map((data) => (
+				<div css={buttonBoxStyle} key={data.id}>
+					{data.id === "프로필" ? "" : <h3>{data.id}</h3>}
+					{data.button.map((buttonData) => (
+						<Button
+							key={buttonData.text}
+							text={buttonData.text}
+							arrowIcon={buttonData.text !== "현재 버전"}
+							isVersion={buttonData.text === "현재 버전"}
+						/>
+					))}
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default Mypage;
