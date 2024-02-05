8309a725b5d0d14bc0e4440ed9f833a6d80fd376
sikkzz <sling0623@gmail.com>
2024-02-05 19:24:19 +0900

    [TS-241] Design: myinfo page landing styling

--- src/components/MyPage/MyInfo/MyInfo.tsx
+++ src/components/MyPage/MyInfo/MyInfo.tsx
@@ -6,29 +6,46 @@ import {
 	layoutStyle,
 	characterBoxStyle,
 	buttonBoxStyle,
+	dividerStyle,
+	authListStyle,
+	mainBoxStyle,
 } from "@/components/MyPage/MyInfo/MyInfo.style";
 
 const MyInfo = () => {
 	return (
 		<div css={layoutStyle}>
-			<div css={characterBoxStyle}>
-				<button>
-					<p>캐릭터 변경</p>
-				</button>
-			</div>
-			{myInfoButtonData.map((data) => (
-				<div key={data.id} css={buttonBoxStyle}>
-					<h3>{data.id}</h3>
-					{data.button.map((buttonData) => (
-						<Button
-							key={buttonData.text}
-							text={buttonData.text}
-							subText={buttonData.subText}
-							isSubText={true}
-						/>
-					))}
+			<div css={mainBoxStyle}>
+				<div css={characterBoxStyle}>
+					<button>
+						<p>캐릭터 변경</p>
+					</button>
 				</div>
-			))}
+
+				{myInfoButtonData.map((data) => (
+					<div key={data.id} css={buttonBoxStyle}>
+						<h3>{data.id}</h3>
+						{data.button.map((buttonData) => (
+							<Button
+								key={buttonData.text}
+								text={buttonData.text}
+								subText={buttonData.subText}
+								isSubText={true}
+							/>
+						))}
+					</div>
+				))}
+			</div>
+
+			<div css={dividerStyle} />
+
+			<ul css={authListStyle}>
+				<li>
+					<p>로그아웃</p>
+				</li>
+				<li>
+					<p>회원탈퇴</p>
+				</li>
+			</ul>
 		</div>
 	);
 };
