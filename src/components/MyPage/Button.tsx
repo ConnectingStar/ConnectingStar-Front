90e4d2db7ff7c55068a7a1dfe66ecc447c4fa9f0
sikkzz <sling0623@gmail.com>
2024-02-05 18:03:50 +0900

    Design: mypage landing styling

---
+++ src/components/MyPage/Button.tsx
@@ -0,0 +1,25 @@
+import RightArrowIcon from "@/assets/icon/ic-right-arrow.svg?react";
+import { layoutStyle, textStyle, versionTextStyle } from "@/components/MyPage/Button.style";
+
+interface buttonProps {
+	icon?: JSX.Element;
+	text: string;
+	arrowIcon: boolean;
+	isVersion: boolean;
+}
+
+const Button = ({ icon, text, arrowIcon, isVersion }: buttonProps) => {
+	return (
+		<div css={layoutStyle}>
+			{icon}
+			<p css={textStyle}>
+				{text}
+				{isVersion && " 1.01.2"}
+				{isVersion && <p css={versionTextStyle}>최신 버전 사용 중</p>}
+			</p>
+			<div>{arrowIcon && <RightArrowIcon />}</div>
+		</div>
+	);
+};
+
+export default Button;
