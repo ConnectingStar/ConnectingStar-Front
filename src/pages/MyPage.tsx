aa026de595ea76f379d44f8af2d335b4f59908f6
sikkzz <sling0623@gmail.com>
2024-02-05 18:09:13 +0900

    [TS-237] Refactor: mypage component directory change

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,7 +1,13 @@
 import Gnb from "@/components/common/Gnb/Gnb";
+import Landing from "@/components/MyPage/Landing/Landing";
 
 const MyPage = () => {
-	return <Gnb />;
+	return (
+		<>
+			<Landing />
+			<Gnb />
+		</>
+	);
 };
 
 export default MyPage;
