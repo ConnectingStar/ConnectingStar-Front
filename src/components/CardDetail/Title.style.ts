deafee2566c04045c3268150dc65bb123dda4c4b
khkh0109 <ist200120@gmail.com>
2024-02-07 02:39:12 +0900

    [TS-244] Style: Title 컴포넌트 추가

---
+++ src/components/CardDetail/Title.style.ts
@@ -0,0 +1,13 @@
+import { css } from "@emotion/react";
+
+export const titleStyle = {
+	h1: css`
+		font-size: 1.5rem;
+		font-weight: 500;
+		color: #111;
+	`,
+	h2: css`
+		font-weight: 500;
+		color: #0176f9;
+	`,
+};
---
+++ src/components/CardDetail/Title.tsx
@@ -0,0 +1,12 @@
+import { titleStyle } from "@/components/CardDetail/Title.style";
+
+interface TitleProps {
+	as: "h1" | "h2";
+	children: React.ReactNode;
+}
+
+export default function Title({ as, children }: TitleProps) {
+	const As = as;
+
+	return <As css={titleStyle[as]}>{children}</As>;
+}
