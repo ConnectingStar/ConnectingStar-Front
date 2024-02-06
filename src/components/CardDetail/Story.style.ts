a3f0550bd75fded61c52ce2d67c37b7ee73b0d74
khkh0109 <ist200120@gmail.com>
2024-02-06 22:10:19 +0900

    [TS-244] Style: Story 컴포넌트 추가

---
+++ src/components/CardDetail/Story.style.ts
@@ -0,0 +1,20 @@
+import { css } from "@emotion/react";
+
+export const storyStyle = css`
+	display: flex;
+	flex-direction: column;
+`;
+
+export const titleStyle = css`
+	color: #767676;
+	font-size: 0.875rem;
+	font-weight: 700;
+`;
+
+export const descriptionStyle = css`
+	height: 11.9375rem;
+	padding: 1rem;
+	border-radius: 0.9375rem;
+	color: #111111;
+	background-color: #f7f7fb;
+`;
