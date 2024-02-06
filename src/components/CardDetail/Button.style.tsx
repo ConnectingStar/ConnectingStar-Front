57e6127dcfc510f057a0be7ace51d494204bfd66
khkh0109 <ist200120@gmail.com>
2024-02-06 22:11:46 +0900

    [TS-244] Refactor: Button 컴포넌트와 스타일 분리

---
+++ src/components/CardDetail/Button.style.tsx
@@ -0,0 +1,12 @@
+// TODO: Button 공통 컴포넌트 만들어지면 교체 예정
+
+import { css } from "@emotion/react";
+
+export const buttonStyle = css`
+	width: 100%;
+	padding: 1rem 0;
+	border-radius: 0.9375rem;
+	font-weight: 500;
+	color: #fff;
+	background-color: #0176f9;
+`;
--- src/components/CardDetail/Button.tsx
+++ src/components/CardDetail/Button.tsx
@@ -1,7 +1,6 @@
 // TODO: Button 공통 컴포넌트 만들어지면 교체 예정
 
-import { css } from "@emotion/react";
-
+import { buttonStyle } from "./Button.style";
 interface ButtonProps {
 	children: React.ReactNode;
 }
@@ -13,12 +12,3 @@ export default function Button({ children }: ButtonProps) {
 		</button>
 	);
 }
-
-const buttonStyle = css`
-	width: 100%;
-	padding: 1rem 0;
-	border-radius: 0.9375rem;
-	font-weight: 500;
-	color: #fff;
-	background-color: #0176f9;
-`;
