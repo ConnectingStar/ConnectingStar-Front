19bfd4da8121f8a986741f1bad56271d153dd4e8
khkh0109 <ist200120@gmail.com>
2024-02-07 20:25:21 +0900

    [TS-244] Style: 가로 360px 제한

--- src/components/CardDetail/CardDetailPage.style.ts
+++ src/components/CardDetail/CardDetailPage.style.ts
@@ -1,7 +1,9 @@
 import { css } from "@emotion/react";
 
 export const sectionStyle = css`
+	width: 360px;
 	padding: 1.25rem 1.5rem 0 1.5rem;
+	margin: 0 auto;
 `;
 
 // TODO: Header 컴포넌트 머지 되면 교체 예정
@@ -11,7 +13,9 @@ export const headerStyle = css`
 
 // TODO: 공통 버튼 컴포넌트 추가되면 교체 예정
 export const buttonContainerStyle = css`
+	width: 360px;
 	padding: 1rem 1.5rem;
+	margin: 0 auto;
 	position: sticky;
 	bottom: 0;
 	background-color: #fff;
