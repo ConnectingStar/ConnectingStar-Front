47b351b620186903b3e4a7d12d462ce3a1c5df90
khkh0109 <ist200120@gmail.com>
2024-02-07 19:19:11 +0900

    [TS-244] Refactor: props 전달 값 하드 코딩
    명확한 데이터 구조를 모르기 때문에 우선 하드코딩 함

--- src/components/CardDetail/Img/Img.tsx
+++ src/components/CardDetail/Img/Img.tsx
@@ -4,26 +4,20 @@ import Star from "@/components/CardDetail/Img/Star";
 
 // TODO: API 연결 후 삭제 예정
 interface dataType {
-	image: string;
-	name: string;
 	state: "default" | "selected" | "possession";
-	star: number;
 }
 
 // TODO: API 연결 후 삭제 예정
 const data: dataType = {
-	image: "",
-	name: "미어캣 별자리",
 	state: "default",
-	star: 10,
 };
 
 export default function Img() {
 	return (
 		<div css={imgStyle} className={data.state === "selected" ? "selected" : ""}>
-			<img src={data.image} alt={data.name} />
+			<img src="" alt="별자리" />
 			{data.state === "possession" && <PossessionLabel />}
-			<Star starNumber={data.star} />
+			<Star starNumber={10} />
 		</div>
 	);
 }
