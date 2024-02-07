9a33d3a2dac03bf4e42f1618f2bbbb6f98d18b03
khkh0109 <ist200120@gmail.com>
2024-02-07 17:23:00 +0900

    [TS-244] Feat: PossessionLabel 컴포넌트 추가

---
+++ src/components/CardDetail/PossessionLabel.style.ts
@@ -0,0 +1,13 @@
+import { css } from "@emotion/react";
+
+export const possessionLabelStyle = css`
+	width: 100px;
+	padding: 16px 0;
+	border-radius: 15px 0 15px 0;
+	position: absolute;
+	top: 0;
+	font-weight: 700;
+	text-align: center;
+	color: #fff;
+	background-color: #385678;
+`;
---
+++ src/components/CardDetail/PossessionLabel.tsx
@@ -0,0 +1,5 @@
+import { possessionLabelStyle } from "@/components/CardDetail/PossessionLabel.style";
+
+export default function PossessionLabel() {
+	return <div css={possessionLabelStyle}>보유</div>;
+}
