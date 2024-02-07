33ca827feb2e625cafeb6513802abebed2eb6e73
khkh0109 <ist200120@gmail.com>
2024-02-07 17:20:32 +0900

    [TS-244] Feat: Star 컴포넌트 추가

---
+++ src/components/CardDetail/Star.style.ts
@@ -0,0 +1,18 @@
+import { css } from "@emotion/react";
+
+export const starImgStyle = css`
+	width: 60px;
+	height: 60px;
+	position: absolute;
+	bottom: 16px;
+	right: 16px;
+
+	strong {
+		position: absolute;
+		top: 18px;
+		left: 18px;
+		font-size: 20px;
+		font-weight: 700;
+		color: #111;
+	}
+`;
---
+++ src/components/CardDetail/Star.tsx
@@ -0,0 +1,11 @@
+import starImg from "@/assets/image/card-detail-star.png";
+import { starImgStyle } from "@/components/CardDetail/Star.style";
+
+export default function Star({ children }: { children: React.ReactNode }) {
+	return (
+		<div css={starImgStyle}>
+			<img src={starImg} alt="노란색 별" />
+			<strong>{children}</strong>
+		</div>
+	);
+}
