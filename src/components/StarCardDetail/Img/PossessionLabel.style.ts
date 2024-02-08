985368602e67db622ede2217ab23f62743a8be7a
khkh0109 <ist200120@gmail.com>
2024-02-08 12:35:38 +0900

    [TS-244] Refactor: PossessionLabel font, color를 theme 값으로 교체

--- src/components/StarCardDetail/Img/PossessionLabel.style.ts
+++ src/components/StarCardDetail/Img/PossessionLabel.style.ts
@@ -1,13 +1,15 @@
 import { css } from "@emotion/react";
 
+import { theme } from "@/styles/theme";
+
 export const possessionLabelStyle = css`
 	width: 100px;
 	padding: 16px 0;
 	border-radius: 15px 0 15px 0;
 	position: absolute;
 	top: 0;
-	font-weight: 700;
+	${theme.font.body_a_bold}
 	text-align: center;
 	color: #fff;
-	background-color: #385678;
+	background-color: ${theme.color.main_deepBlue};
 `;
