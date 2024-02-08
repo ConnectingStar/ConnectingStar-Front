96cddb8b43861d03e1be1ca17a057b822aa74542
pizzaYami <cyj9407@naver.com>
2024-02-08 15:30:07 +0900

    [TS-2] Feat: SignUp img에 alt 값 넣어줌

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -5,7 +5,7 @@ import { SignUpStyle } from "@/pages/SignUp/SignUp.style";
 function SignUp() {
 	return (
 		<div css={SignUpStyle}>
-			<img src="" alt="" />
+			<img src="" alt="logo" />
 			<pre>{signUpText[0]}</pre>
 		</div>
 	);
