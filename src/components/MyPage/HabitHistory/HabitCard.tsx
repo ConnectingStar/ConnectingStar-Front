6f6c09b02939b6a8c67dee1f1056b5c339bd9cc5
sikkzz <sling0623@gmail.com>
2024-02-08 22:31:31 +0900

    [TS-254] Design: habit card end, notEnd data styling

--- src/components/MyPage/HabitHistory/HabitCard.tsx
+++ src/components/MyPage/HabitHistory/HabitCard.tsx
@@ -1,14 +1,56 @@
-import { layoutStyle } from "@/components/MyPage/HabitHistory/HabitCard.style";
+import CloseIcon from "@/assets/icon/ic-close.svg?react";
 
-const HabitCard = () => {
+import {
+	layoutStyle,
+	textBoxStyle,
+	textStyle,
+} from "@/components/MyPage/HabitHistory/HabitCard.style";
+
+interface habitCardDataType {
+	isEnd: boolean;
+	title: string;
+	startDate: string;
+	endDate?: string;
+	successCount: number;
+	failCount: number;
+	endReason?: string;
+}
+
+const HabitCard = ({
+	isEnd,
+	title,
+	startDate,
+	endDate,
+	successCount,
+	failCount,
+	endReason,
+}: habitCardDataType) => {
 	return (
 		<div css={layoutStyle}>
-			<h1>오후 8시에 우리 집 안 내 책상 위에서 책 읽기 5 페이지</h1>
-			<p>
-				<span>시작일 : 2023. 12.11</span>
-				<span>실천 : 53</span>
-				<span>휴식 : 13</span>
-			</p>
+			{isEnd && <CloseIcon />}
+			<h1>{title}</h1>
+
+			{isEnd ? (
+				<div css={textBoxStyle}>
+					<p css={textStyle}>
+						<span>시작일 : {startDate}</span>
+						<span>종료일 : {endDate}</span>
+					</p>
+					<p css={textStyle}>
+						<span>실천 : {successCount}</span>
+						<span>휴식 : {failCount}</span>
+					</p>
+					<p css={textStyle}>
+						<span>종료 사유 : {endReason}</span>
+					</p>
+				</div>
+			) : (
+				<p css={textStyle}>
+					<span>시작일 : {startDate}</span>
+					<span>실천 : {successCount}</span>
+					<span>휴식 : {failCount}</span>
+				</p>
+			)}
 		</div>
 	);
 };
