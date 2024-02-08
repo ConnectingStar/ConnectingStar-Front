2f1aa4fd02de97d71b570484c713b4cca25a8e94
sikkzz <sling0623@gmail.com>
2024-02-09 01:35:38 +0900

    [TS-241] Feat: my info page header add

--- src/pages/MyInfoPage.tsx
+++ src/pages/MyInfoPage.tsx
@@ -1,7 +1,16 @@
+import Header from "@/components/common/Header/Header";
 import MyInfo from "@/components/MyPage/MyInfo/MyInfo";
 
 const MyInfoPage = () => {
-	return <MyInfo />;
+	return (
+		<>
+			<Header>
+				<Header.PrevButton />
+				<Header.Title>내 정보</Header.Title>
+			</Header>
+			<MyInfo />;
+		</>
+	);
 };
 
 export default MyInfoPage;
