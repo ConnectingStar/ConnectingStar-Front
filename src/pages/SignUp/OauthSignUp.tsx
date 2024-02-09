44ef7a8ddca75f18627be98dbdb3f311af036d36
pizzaYami <cyj9407@naver.com>
2024-02-10 01:03:54 +0900

    [TS-2] Rename: OauthSignUp -> oauthSignUp

--- src/pages/SignUp/OauthSignUp.tsx
+++ src/pages/SignUp/OauthSignUp.tsx
@@ -1,15 +1,15 @@
 import google from "@/assets/icon/ic-signup.google.svg";
 import kakao from "@/assets/icon/ic-signup.kakao.svg";
 
-import { OauthSignUpStyle } from "@/pages/SignUp/OauthSignUp.style";
+import { oauthSignUpStyle } from "@/pages/SignUp/OauthSignUp.style";
 function OauthSignUp() {
 	return (
-		<div css={OauthSignUpStyle.container}>
-			<div css={OauthSignUpStyle.logo}>
+		<div css={oauthSignUpStyle.container}>
+			<div css={oauthSignUpStyle.logo}>
 				<img src="" alt="logo" />
 				<p>로고</p>
 			</div>
-			<div css={OauthSignUpStyle.oauth}>
+			<div css={oauthSignUpStyle.oauth}>
 				<button type="button">
 					<img src={kakao} alt="kakao" />
 					카카오로 계속하기
@@ -19,7 +19,7 @@ function OauthSignUp() {
 					구글로 계속하기
 				</button>
 			</div>
-			<footer css={OauthSignUpStyle.footer}>
+			<footer css={oauthSignUpStyle.footer}>
 				<p>서비스를 시작하시면 다음 항목에 동의하게 돼요</p>
 				<div>
 					<a className="line">서비스 이용약관</a>
