09b2631a61fd0d5786aa1d70bf1a933c9a247549
sikkzz <sling0623@gmail.com>
2024-02-11 23:54:26 +0900

    [TS-256] Feat: create leave reason modal

--- src/components/MyPage/Withdrawal/Withdrawal.tsx
+++ src/components/MyPage/Withdrawal/Withdrawal.tsx
@@ -1,20 +1,24 @@
-import { useAppDispatch } from "@/api/hooks";
+import { useAppSelector, useAppDispatch } from "@/api/hooks";
 import { openModal } from "@/api/modal/modalSlice";
 import DownArrowIcon from "@/assets/icon/ic-down-arrow.svg?react";
+import LeaveReasonModal from "@/components/MyPage/Withdrawal/LeaveResonModal/LeaveReasonModal";
 
 import { layoutStyle, reasonBoxStyle } from "@/components/MyPage/Withdrawal/Withdrawal.style";
 
 const Withdrawal = () => {
 	const dispatch = useAppDispatch();
 
+	const { modal } = useAppSelector((state) => state.modal);
+
 	return (
 		<div css={layoutStyle}>
 			<h1>어떤 이유로 탈퇴하시나요?</h1>
-			<div css={reasonBoxStyle} onClick={() => dispatch(openModal("auth"))}>
+			<div css={reasonBoxStyle} onClick={() => dispatch(openModal("leaveReason"))}>
 				<p>탈퇴 이유를 선택해 주세요</p>
 				<DownArrowIcon />
 			</div>
 			<button type="button">작별하기</button>
+			{modal === "leaveReason" && <LeaveReasonModal />}
 		</div>
 	);
 };
