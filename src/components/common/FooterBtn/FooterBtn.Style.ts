ce5546a17a3ba16e116a4b6879721d7362ba60ce
pizzaYami <cyj9407@naver.com>
2024-02-15 17:09:38 +0900

    [TS-250] Feat: FooterBtn 합성컴포넌트 제거

--- src/components/common/FooterBtn/FooterBtn.Style.ts
+++ src/components/common/FooterBtn/FooterBtn.Style.ts
@@ -2,58 +2,61 @@ import { css } from "@emotion/react";
 
 import { theme } from "@/styles/theme";
 
-const FooterBtnStyle = {
-	container: css`
-		max-width: 500px;
-		width: 100%;
+export const FooterBtnRound = (blur: boolean) => {
+	return css`
 		position: fixed;
 		bottom: 0;
+		width: 360px;
+		height: 88px;
+		padding: 17px 24px;
 		display: flex;
 		align-items: center;
 		justify-content: center;
-	`,
-	round: css`
-		box-sizing: content-box;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		max-width: 500px;
-		width: 100%;
-		height: 55px;
-		padding: 16px 24px;
 		gap: 8px;
+		left: 50%;
+		transform: translateX(-50%);
+
 		& > button {
 			width: 100%;
 			height: 55px;
 			border-radius: 15px;
 			${theme.font.button_big};
-			background-color: ${theme.color.main_Blue};
-			color: white;
-		}
-		.canclebutton {
-			background-color: ${theme.color.button_disabled};
-			color: ${theme.color.button_deactivated};
-		}
-	`,
-	square: css`
-		display: flex;
-		position: fixed;
-		bottom: 0;
-		left: 0;
-		width: 100%;
-		height: 55px;
-		${theme.font.button_big}
-		& > button {
-			width: 100%;
-			height: 100%;
+			opacity: ${blur ? "40%" : "100%"};
 			color: white;
 			background-color: ${theme.color.main_Blue};
+
+			&.cancle {
+				color: ${theme.color.button_deactivated};
+				background-color: ${theme.color.button_disabled};
+				opacity: 100%;
+			}
 		}
-		.canclebutton {
-			background-color: ${theme.color.button_disabled};
-			color: ${theme.color.button_deactivated};
-		}
-	`,
+	`;
 };
 
-export default FooterBtnStyle;
+export const FooterBtnSquare = (blur: boolean) => {
+	return css`
+		width: 100%;
+		height: 3.44rem;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		position: fixed;
+		left: 0;
+		bottom: 0;
+
+		& > button {
+			height: 100%;
+			background-color: ${theme.color.main_Blue};
+			color: white;
+			opacity: ${blur ? "40%" : "100%"};
+			flex-grow: 1;
+
+			&.cancle {
+				color: ${theme.color.button_deactivated};
+				background-color: ${theme.color.button_disabled};
+				opacity: 100%;
+			}
+		}
+	`;
+};
