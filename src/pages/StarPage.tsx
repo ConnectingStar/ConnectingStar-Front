453197e694563f8ca40c8f98d5a70ad94017cac9
khkh0109 <ist200120@gmail.com>
2024-02-15 17:57:14 +0900

    [TS-267] Feat: StarPage에 Header 컴포넌트 추가

--- src/pages/StarPage.tsx
+++ src/pages/StarPage.tsx
@@ -1,7 +1,16 @@
 import Gnb from "@/components/common/Gnb/Gnb";
+import Header from "@/components/common/Header";
 
 const StarPage = () => {
-	return <Gnb />;
+	return (
+		<>
+			<Header>
+				<Header.PrevButton />
+				<Header.Title>별자리 카드</Header.Title>
+			</Header>
+			<Gnb />
+		</>
+	);
 };
 
 export default StarPage;
