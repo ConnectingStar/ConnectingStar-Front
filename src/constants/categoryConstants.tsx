2d3ff4105c0ddbc467a2ffefd1b41c519cf834f0
khkh0109 <ist200120@gmail.com>
2024-02-15 21:30:42 +0900

    [TS-267] Feat: CategoryTab 컴포넌트 추가

---
+++ src/constants/categoryConstants.tsx
@@ -0,0 +1,8 @@
+export const categoryData = [
+	{ id: 0, content: "전체" },
+	{ id: 1, content: "육체 활동" },
+	{ id: 2, content: "마음 강화" },
+	{ id: 3, content: "지식 습득" },
+	{ id: 4, content: "기록 습관" },
+	{ id: 5, content: "휴식ㅣ취미" },
+];
--- src/pages/StarPage.tsx
+++ src/pages/StarPage.tsx
@@ -1,5 +1,6 @@
 import Gnb from "@/components/common/Gnb/Gnb";
 import Header from "@/components/common/Header";
+import CategoryTab from "@/components/StarPage/CategoryTab";
 
 const StarPage = () => {
 	return (
@@ -8,6 +9,7 @@ const StarPage = () => {
 				<Header.PrevButton />
 				<Header.Title>별자리 카드</Header.Title>
 			</Header>
+			<CategoryTab />
 			<Gnb />
 		</>
 	);
