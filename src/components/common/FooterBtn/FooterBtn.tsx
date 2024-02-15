216658116376ca215b464bf772b962769d029834
pizzaYami <cyj9407@naver.com>
2024-02-15 17:38:37 +0900

    [TS-250] Feat: transparent추가, cancleBtn 삭제 cancleText 하나만 사용

--- src/components/common/FooterBtn/FooterBtn.tsx
+++ src/components/common/FooterBtn/FooterBtn.tsx
@@ -2,18 +2,26 @@ import { FooterBtnRound, FooterBtnSquare } from "@/components/common/FooterBtn/F
 
 interface FooterBtnType {
 	text: string;
+	cancleText?: string;
 	shape: string;
 	blur?: boolean;
-	cancleBtn?: boolean;
+	transparent?: boolean;
 }
 
-// text, blur, cancleBtn, shape = "square" or "round"
-export default function FooterBtn({ text, shape, blur, cancleBtn }: FooterBtnType) {
+// text, cancleText blur, cancleBtn, shape = "square" or "round"
+// backgournd-color: transparent
+export default function FooterBtn({ text, cancleText, shape, blur, transparent }: FooterBtnType) {
 	return (
-		<div css={shape === "round" ? FooterBtnRound(blur === true) : FooterBtnSquare(blur === true)}>
-			{cancleBtn ? (
+		<div
+			css={
+				shape === "round"
+					? FooterBtnRound(blur === true, transparent === true)
+					: FooterBtnSquare(blur === true)
+			}
+		>
+			{cancleText ? (
 				<button type="button" className="cancle">
-					취소
+					{cancleText}
 				</button>
 			) : null}
 			<button type="button">{text}</button>
