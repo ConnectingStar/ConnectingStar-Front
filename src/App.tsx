586ead1b73bd08b053c68d0b99fd23a17d252666
pizzaYami <cyj9407@naver.com>
2024-02-16 19:30:25 +0900

    [TS-53] Feat: CreateAccountPage App에 추가

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,8 @@ import StarCardDetailPage from "@/pages/StarCardDetailPage";
 import StarPage from "@/pages/StarPage";
 import WithdrawalPage from "@/pages/WithdrawalPage";
 
+import CreateAccountPage from "./pages/CreateAccountPage";
+
 const App = () => {
 	return (
 		<BrowserRouter>
@@ -26,6 +28,7 @@ const App = () => {
 				<Route path="/mypage" element={<MyPage />} />
 				<Route path="/myinfo" element={<MyInfoPage />} />
 				<Route path="/signup" element={<SignUp />} />
+				<Route path="/createAccount" element={<CreateAccountPage />} />
 				<Route path="/oauthsignup" element={<OauthSignUp />} />
 				<Route path="/createAccount" element={<CreateAccountPage />} />
 				<Route path="/oauthsignup" element={<OauthSignUp />} />
