5d768eeefff3ecbff6fc70c6325b2a40d6ef6eb7
pizzaYami <cyj9407@naver.com>
2024-02-18 22:00:06 +0900

    [TS-53] Feat: DiscoveryRoutePage 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import ChartPage from "@/pages/ChartPage";
 import CreateAccountPage from "@/pages/CreateAccountPage";
-import CreateAccountPage from "@/pages/CreateAccountPage";
 import HomePage from "@/pages/HomePage";
 import MyHabitPage from "@/pages/MyHabitPage";
 import MyInfoPage from "@/pages/MyInfoPage";
@@ -27,7 +26,6 @@ const App = () => {
 				<Route path="/mypage" element={<MyPage />} />
 				<Route path="/myinfo" element={<MyInfoPage />} />
 				<Route path="/signup" element={<SignUp />} />
-				<Route path="/createAccount" element={<CreateAccountPage />} />
 				<Route path="/oauthsignup" element={<OauthSignUp />} />
 				<Route path="/createAccount" element={<CreateAccountPage />} />
 				<Route path="/oauthsignup" element={<OauthSignUp />} />
