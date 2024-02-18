a3be34a73bf886f2c78825aa1be20766544dde51
pizzaYami <cyj9407@naver.com>
2024-02-18 23:06:34 +0900

    [TS-53] Rename: DiscoveryRoutePage -> VisitTrackPage

---
+++ src/pages/VisitTrackPage.tsx
@@ -0,0 +1,24 @@
+import Header from "@/components/common/Header";
+
+import { visitTrackStyle } from "./VisitTrackPage.style";
+
+function VisitTrackPage() {
+	const discoveryRouteText = ["앱 스토어", "지인 추천", "SNS 광고", "검색", "기타"];
+
+	return (
+		<>
+			<Header>
+				<Header.PrevButton></Header.PrevButton>
+			</Header>
+			<div css={visitTrackStyle}>
+				<h1>어떤 경로를 통해 오셨나요?</h1>
+				<ul>
+					{discoveryRouteText.map((item) => (
+						<li>{item}</li>
+					))}
+				</ul>
+			</div>
+		</>
+	);
+}
+export default VisitTrackPage;
