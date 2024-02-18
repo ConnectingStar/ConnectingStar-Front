698c07508972bfb40c80a6fed58910c253c462fb
sikkzz <sling0623@gmail.com>
2024-02-19 01:24:08 +0900

    [TS-272] Design: stop habit modal button add

--- src/components/MyPage/NotificationSetting/StopHabitModal/StopHabitModal.tsx
+++ src/components/MyPage/NotificationSetting/StopHabitModal/StopHabitModal.tsx
@@ -1,15 +1,30 @@
+import { useAppDispatch } from "@/api/hooks";
+import { closeModal } from "@/api/modal/modalSlice";
 import Modal from "@/components/common/Modal/Modal";
+import Calender from "@/components/MyPage/NotificationSetting/StopHabitModal/Calendar/Calender";
 
-import Calender from "./Calendar/Calender";
-
-import { layoutStyle } from "@/components/MyPage/NotificationSetting/StopHabitModal/StopHabitModal.style";
+import {
+	layoutStyle,
+	buttonBoxStyle,
+} from "@/components/MyPage/NotificationSetting/StopHabitModal/StopHabitModal.style";
 
 const StopHabitModal = () => {
+	const dispatch = useAppDispatch();
+
 	return (
 		<Modal isBottomSheet>
 			<div css={layoutStyle}>
 				<h1>일시 정지 기간을 선택해주세요</h1>
 				<Calender />
+
+				<div css={buttonBoxStyle}>
+					<button type="button" className="cancel" onClick={() => dispatch(closeModal())}>
+						취소
+					</button>
+					<button type="button" onClick={() => dispatch(closeModal())}>
+						선택 완료
+					</button>
+				</div>
 			</div>
 		</Modal>
 	);
