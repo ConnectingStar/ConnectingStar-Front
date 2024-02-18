c82224b6cb49dd7daf1e27e4118e348cbe3a737a
sikkzz <sling0623@gmail.com>
2024-02-18 17:57:31 +0900

    [TS-271] Feat: create delete history modal

---
+++ src/components/MyPage/HabitHistory/DeleteHistoryModal.tsx
@@ -0,0 +1,29 @@
+import { useAppDispatch } from "@/api/hooks";
+import { closeModal } from "@/api/modal/modalSlice";
+import Modal from "@/components/common/Modal/Modal";
+
+import {
+	layoutStyle,
+	buttonBoxStyle,
+} from "@/components/MyPage/MyInfo/LogoutModal/LogoutModal.style";
+
+const DeleteHistoryModal = () => {
+	const dispatch = useAppDispatch();
+
+	return (
+		<Modal>
+			<div css={layoutStyle}>
+				<h1>지난 히스토리를 삭제할까요?</h1>
+
+				<div css={buttonBoxStyle}>
+					<button className="cancel" onClick={() => dispatch(closeModal())}>
+						취소
+					</button>
+					<button>삭제</button>
+				</div>
+			</div>
+		</Modal>
+	);
+};
+
+export default DeleteHistoryModal;
