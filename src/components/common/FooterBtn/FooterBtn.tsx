010be4c39a151238bb3ae67b6a22ae17bdb6d1d5
pizzaYami <cyj9407@naver.com>
2024-02-18 21:17:49 +0900

    [TS-250] Fix: isSquare 고침

--- src/components/common/FooterBtn/FooterBtn.tsx
+++ src/components/common/FooterBtn/FooterBtn.tsx
@@ -10,12 +10,11 @@ interface FooterBtnType {
 }
 
 // text: 기본 버튼 텍스트
-// LeftText: 사이드 버튼 텍스트
-// shape: 버튼 모양 "round" or "square"
-// blur: 버튼 흐리게
+// leftText: 사이드 버튼 텍스트
+// isSquare: 버튼 모양 (default : round)
+// disabled: 버튼 비활성화(blur)
 // transparent : 배경 투명하게 (default: white)
 // positionStatic : position: fixed 제거
-// disabled
 export default function FooterBtn({
 	text,
 	leftText,
@@ -27,7 +26,7 @@ export default function FooterBtn({
 	return (
 		<div
 			css={
-				isSquare ? FooterBtnRound(transparent === true, positionStatic === true) : FooterBtnSquare
+				isSquare ? FooterBtnSquare : FooterBtnRound(transparent === true, positionStatic === true)
 			}
 		>
 			{leftText && (
