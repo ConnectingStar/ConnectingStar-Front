b47755c19adcad4a30a82ab2df1f378bca9a98a2
sikkzz <sling0623@gmail.com>
2024-02-19 16:20:50 +0900

    [TS-253] Design: sort button styling

---
+++ src/components/MyPage/StarTrace/SortButton.style.ts
@@ -0,0 +1,36 @@
+import { css } from "@emotion/react";
+
+import { theme } from "@/styles/theme";
+
+export const layoutStyle = css`
+	display: flex;
+	align-items: center;
+	gap: 6px;
+	padding: 0.4375rem 1.5rem 0;
+`;
+
+export const sortButtonStyle = css`
+	padding: 0.5rem 1rem;
+	display: flex;
+	align-items: center;
+	gap: 4px;
+	border-radius: 20px;
+	border: 2px solid ${theme.color.button_disabled};
+
+	& > p {
+		color: ${theme.color.font_black};
+		${theme.font.body_b};
+	}
+
+	& > svg {
+		margin-top: 1px;
+	}
+`;
+
+export const buttonStyle = css`
+	padding: 0.5rem 1rem;
+	border-radius: 20px;
+	background-color: ${theme.color.main_deepBlue};
+	color: #fff;
+	${theme.font.body_b};
+`;
