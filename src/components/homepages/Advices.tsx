13d172b41281fb02aad0d03ce3088c7510e3c8c1
김영택 <kooduck168@gmail.com>
2024-02-19 16:45:22 +0900

    [TS-4] Feat: Advices 이미지 자동 캐러셀 및 style 수정

--- src/components/homepages/Advices.tsx
+++ src/components/homepages/Advices.tsx
@@ -1,16 +1,45 @@
-import { ReactNode } from "react";
+import { useState, useRef, useEffect } from "react";
 
-import { css } from "@emotion/react";
+import banner01 from "@/assets/image/img-homepage-banner-01.png";
+import banner02 from "@/assets/image/img-homepage-banner-02.png";
+import banner03 from "@/assets/image/img-homepage-banner-03.png";
+import banner04 from "@/assets/image/img-homepage-banner-04.png";
+import banner05 from "@/assets/image/img-homepage-banner-05.png";
 
-import { AdvicesStyle } from "./Advices.style";
+import { advicesStyle } from "@/components/homepages/Advices.style";
 
-function Advices(): ReactNode {
+function Advices() {
+	const containerRef = useRef<HTMLDivElement | null>(null);
+	const images: string[] = [banner01, banner02, banner03, banner04, banner05];
+	const [currentIndex, setCurrentIndex] = useState<number>(0);
+	useEffect(() => {
+		containerRef.current?.scrollTo({
+			left:
+				(containerRef.current?.scrollWidth - containerRef.current?.offsetWidth) *
+				(currentIndex / 4),
+			behavior: "smooth",
+		});
+	}, [currentIndex]);
+
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			setCurrentIndex((currentIndex + 1) % 5);
+		}, 4000);
+		return () => clearInterval(intervalId);
+	}, [currentIndex]);
+
 	return (
-		<section
-			css={css`
-				${AdvicesStyle.container}
-			`}
-		></section>
+		<section css={advicesStyle.container} ref={containerRef}>
+			<ul css={advicesStyle.advicesCarousel}>
+				{images.map((image, idx) => {
+					return (
+						<li css={advicesStyle.adviceWrapper} key={idx}>
+							<img src={image}></img>
+						</li>
+					);
+				})}
+			</ul>
+		</section>
 	);
 }
 
