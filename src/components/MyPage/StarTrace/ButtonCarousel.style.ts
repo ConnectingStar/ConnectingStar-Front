53899c9ef557b65610a39ff7eb974260aa3ac6bd
sikkzz <sling0623@gmail.com>
2024-02-19 20:46:38 +0900

    [TS-253] Design: button carousel styling

---
+++ src/components/MyPage/StarTrace/ButtonCarousel.style.ts
@@ -0,0 +1,30 @@
+import { css } from "@emotion/react";
+
+import { theme } from "@/styles/theme";
+
+export const flexStyle = css`
+	overflow: hidden;
+	white-space: nowrap;
+	padding: 0.5rem 1.5rem 0;
+`;
+
+export const scrollBoxStyle = css`
+	display: inline-block;
+	white-space: nowrap;
+	transition-duration: 0.15s;
+	transition-timing-function: cubic-bezier(0.05, 0, 0, 1);
+	will-change: transform;
+	overflow: hidden;
+`;
+
+export const getButtonStyle = (isActive: boolean) => {
+	return css`
+		padding: 1rem;
+		border-radius: 0.9375rem;
+		background-color: ${theme.color.main_Blue};
+		background-color: ${isActive ? theme.color.main_Blue : theme.color.button_disabled};
+		${theme.font.body_a_bold};
+		color: ${isActive ? "#fff" : theme.color.button_deactivated};
+		margin-right: 6px;
+	`;
+};
