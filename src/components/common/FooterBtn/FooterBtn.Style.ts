7f52efcfb650e15f64d94f96813fae9b58add5fa
pizzaYami <cyj9407@naver.com>
2024-02-19 20:32:50 +0900

    [TS-250] Refactor: FooterBtn

--- src/components/common/FooterBtn/FooterBtn.Style.ts
+++ src/components/common/FooterBtn/FooterBtn.Style.ts
@@ -8,10 +8,6 @@ export const footerBtnStyle = (
 	isPositionStatic?: boolean,
 ) => {
 	return css`
-		${isPositionStatic ? null : "position: fixed;bottom: 0;left: 50%;	transform: translateX(-50%);"}
-		width: 22.5rem;
-		height: 5.5rem;
-		padding: 1.063rem 1.5rem;
 		display: flex;
 		align-items: center;
 		justify-content: center;
@@ -39,37 +35,6 @@ export const footerBtnStyle = (
 			&.cancel {
 				color: ${theme.color.button_deactivated};
 				background-color: ${theme.color.button_disabled};
-				opacity: 100%;
-			}
-		}
-	`;
-};
-
-export const FooterBtnSquare = () => {
-	return css`
-		width: 100%;
-		height: 3.44rem;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		position: fixed;
-		left: 0;
-		bottom: 0;
-
-		& > button {
-			height: 100%;
-			background-color: ${theme.color.main_blue};
-			color: white;
-			flex-grow: 1;
-
-			&:disabled {
-				opacity: 40%;
-			}
-
-			&.cancle {
-				color: ${theme.color.button_deactivated};
-				background-color: ${theme.color.button_disabled};
-				opacity: 100%;
 			}
 		}
 	`;
