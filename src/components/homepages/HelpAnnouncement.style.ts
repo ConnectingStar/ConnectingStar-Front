062d5ee0549c014dfc341522567dbe2cee751c48
김영택 <kooduck168@gmail.com>
2024-02-19 16:51:09 +0900

    [TS-4] Style: HelpAnnouncement 단위 rem 수정

--- src/components/homepages/HelpAnnouncement.style.ts
+++ src/components/homepages/HelpAnnouncement.style.ts
@@ -2,23 +2,22 @@ import { css } from "@emotion/react";
 
 import { theme } from "@/styles/theme";
 
-export const HelpAnnouncementStyle = {
+export const helpAnnouncementStyle = {
 	container: css`
 		display: flex;
 		align-items: center;
 		width: 100%;
 		height: 3.5rem; // 56px
 		background-color: ${theme.color.line};
-		border-radius: 28px;
+		border-radius: 15px;
 		padding: 0 0.875rem 0 0.875rem; // 0 14px 0 14px
-		margin-bottom: 1.25rem; // 20px
 	`,
 	iconWrapper: css`
 		display: flex;
 		justify-content: center;
 		align-items: center;
 		width: auto;
-		height: 100%;
+		height: 2rem;
 		aspect-ratio: 1/1;
 	`,
 	lineWrapper: css`
