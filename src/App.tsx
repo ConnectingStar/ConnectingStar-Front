ec99a3d6b09c15b7a70d403bfe7153758302b13c
pizzaYami <cyj9407@naver.com>
2024-02-20 09:11:43 +0900

    [TS-25] Init: 초기설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import ChartPage from "@/pages/ChartPage";
+import ChatPage from "@/pages/ChatPage";
 import HabitDeletePage from "@/pages/HabitDeletePage";
 import HomePage from "@/pages/HomePage";
 import MyHabitPage from "@/pages/MyHabitPage";
@@ -33,6 +34,7 @@ const App = () => {
 				<Route path="/withdrawal" element={<WithdrawalPage />} />
 				<Route path="/rest-record" element={<RestRecordPage />} />
 				<Route path="/habit-delete" element={<HabitDeletePage />} />
+				<Route path="/chat" element={<ChatPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
