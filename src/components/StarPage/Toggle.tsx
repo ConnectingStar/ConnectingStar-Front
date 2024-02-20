407d8b4f3f10262ca9a5a395493a24a3c86acde3
khkh0109 <ist200120@gmail.com>
2024-02-20 18:20:14 +0900

    [TS-267] Feat: Toggle 컴포넌트 추가

---
+++ src/components/StarPage/Toggle.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+import { css } from "@emotion/react";
+
+import { theme } from "@/styles/theme";
+
+export default function Toggle() {
+	const [isToggleActive, setIsToggleActive] = useState(false);
+
+	return (
+		<div css={containerStyle}>
+			<p>보유 캐릭터 모아보기</p>
+			<div
+				css={getToggleStyle(isToggleActive)}
+				onClick={() => setIsToggleActive(!isToggleActive)}
+			></div>
+		</div>
+	);
+}
+
+const containerStyle = css`
+	display: flex;
+	justify-content: flex-end;
+	align-items: center;
+	gap: 12px;
+	${theme.font.body_b_bold}
+`;
+
+const getToggleStyle = (isToggleActive: boolean) => css`
+	width: 2.875rem;
+	height: 1.5rem;
+	padding: 0.125rem;
+	border-radius: 1rem;
+	background-color: ${isToggleActive ? theme.color.main_blue : theme.color.button_deactivated};
+
+	:after {
+		content: "";
+		display: block;
+		width: 1.25rem;
+		height: 1.25rem;
+		border-radius: 100%;
+		margin-left: ${isToggleActive ? "calc(100% - 1.25rem)" : "0"};
+		background-color: #fff;
+		transition: margin-left 0.5s ease;
+	}
+`;
