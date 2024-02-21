3ee2f9706b8429d85c5c055f70188d59f80fe293
pizzaYami <cyj9407@naver.com>
2024-02-21 12:01:38 +0900

    [TS-250] Rename: onclick -> handleBtnClick

--- src/components/common/FooterBtn/FooterBtn.tsx
+++ src/components/common/FooterBtn/FooterBtn.tsx
@@ -7,8 +7,8 @@ interface FooterBtnType {
 	disabled?: boolean;
 	isTransparent?: boolean;
 	isPositionStatic?: boolean;
-	rightOnclick?: () => void;
-	leftOnclick?: () => void;
+	handleBtnClick?: () => void;
+	handleLeftBtnClick?: () => void;
 }
 
 // text: 기본 버튼 텍스트
@@ -17,8 +17,8 @@ interface FooterBtnType {
 // disabled: 버튼 비활성화(blur)
 // isTransparent : 배경 투명하게 (default: white)
 // isPositionStatic : position: fixed 제거
-// rightOnclick : 우측버튼 onclick
-// leftOnclick : 좌측버튼 onclick
+// handleBtnClick : 우측버튼 onclick
+// handleLeftBtnClick : 좌측버튼 onclick
 export default function FooterBtn({
 	text,
 	leftText,
@@ -26,18 +26,18 @@ export default function FooterBtn({
 	disabled,
 	isTransparent,
 	isPositionStatic,
-	rightOnclick,
-	leftOnclick,
+	handleBtnClick,
+	handleLeftBtnClick,
 }: FooterBtnType) {
 	return (
 		<div css={footerBtnStyle(isTransparent, isSquare, isPositionStatic)}>
 			{leftText && (
-				<button type="button" className="cancel" onClick={leftOnclick}>
+				<button type="button" className="cancel" onClick={handleLeftBtnClick}>
 					{leftText}
 				</button>
 			)}
 
-			<button type="button" disabled={disabled} onClick={rightOnclick}>
+			<button type="button" disabled={disabled} onClick={handleBtnClick}>
 				{text}
 			</button>
 		</div>
