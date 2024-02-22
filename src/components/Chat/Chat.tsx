2bfcb4b9212dc9816a865c0de79cd1f059138532
pizzaYami <cyj9407@naver.com>
2024-02-22 10:44:40 +0900

    [TS-25] Feat: 채팅 올라가는 로직 완성

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
-import { chatStyle } from "./Chat.style";
+import { chatStyle, container, replyStyle } from "./Chat.style";
 
 interface chatType {
 	message: string[];
@@ -9,7 +9,18 @@ interface chatType {
 	isReplyProps: [boolean, React.Dispatch<React.SetStateAction<boolean>>];
 }
 
-function Chat({ message, reply, percentageProps, isReplyProps }: chatType) {
+function Chat({ children }: { children: ReactNode }) {
+	return <div css={chatStyle}>{children}</div>;
+}
+
+export default Chat;
+
+Chat.Message = function ChattingMessage({
+	message,
+	reply,
+	percentageProps,
+	isReplyProps,
+}: chatType) {
 	const [percentage, setPercentage] = percentageProps;
 	const [isReply, setIsReply] = isReplyProps;
 	const [messageIndex, setMessageIndex] = useState(0);
@@ -32,21 +43,19 @@ function Chat({ message, reply, percentageProps, isReplyProps }: chatType) {
 	};
 
 	return (
-		<div css={chatStyle.chat}>
-			<div css={chatStyle.dev}>
-				<div>
-					<img src="" alt="profile" />
-				</div>
-				<ul>
-					{message.slice(0, messageIndex + 1).map((i, index) => (
-						<li key={index}>{i}</li>
-					))}
-					{message.length === messageIndex && <button onClick={handleProgress}>습관 선택</button>}
-				</ul>
+		<div css={container}>
+			<div>
+				<img src="" alt="profile" />
 			</div>
-			{isReply && <div css={chatStyle.user}>{reply}</div>}
+			<ul>
+				{message.slice(0, messageIndex + 1).map((i, index) => (
+					<li key={index} className="message-item">
+						{i}
+					</li>
+				))}
+				{message.length === messageIndex && <button onClick={handleProgress}>습관 선택</button>}
+				{isReply && <div css={replyStyle}>{reply}</div>}
+			</ul>
 		</div>
 	);
-}
-
-export default Chat;
+};
