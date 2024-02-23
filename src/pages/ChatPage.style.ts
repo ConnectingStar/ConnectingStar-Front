d4a2ee8d1f9c7e60ac9a331c6259507c0e1a2005
pizzaYami <cyj9407@naver.com>
2024-02-23 09:28:45 +0900

    [TS-25] Refacter: ChatPage

--- src/pages/ChatPage.style.ts
+++ src/pages/ChatPage.style.ts
@@ -1,8 +1,6 @@
 import { css } from "@emotion/react";
 
-export const chatStyle = {
-	container: css`
-		max-width: 360px;
-		margin: 0 auto;
-	`,
-};
+export const chatStyle = css`
+	max-width: 360px;
+	margin: 0 auto;
+`;
