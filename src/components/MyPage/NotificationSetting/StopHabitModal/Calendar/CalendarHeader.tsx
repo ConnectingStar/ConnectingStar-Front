ed1546dcde500a41c2fcf6eff3cfed6a14bb026e
sikkzz <sling0623@gmail.com>
2024-02-25 17:30:17 +0900

    [TS-272] Refactor: calendar month disabled add

--- src/components/MyPage/NotificationSetting/StopHabitModal/Calendar/CalendarHeader.tsx
+++ src/components/MyPage/NotificationSetting/StopHabitModal/Calendar/CalendarHeader.tsx
@@ -1,7 +1,8 @@
 import { format } from "date-fns";
 
+import LeftArrowDisabledIcon from "@/assets/icon/ic-calendar-left-arrow-disabled.svg?react";
 import LeftArrowIcon from "@/assets/icon/ic-calendar-left-arrow.svg?react";
-import RightArrowIcon from "@/assets/icon/ic-calender-right-arrow.svg?react";
+import RightArrowIcon from "@/assets/icon/ic-calendar-right-arrow.svg?react";
 
 import {
 	headerLayoutStyle,
@@ -12,16 +13,18 @@ interface calendarHeaderType {
 	currentMonth: Date;
 	onClickPrevMonth: () => void;
 	onClickNextMonth: () => void;
+	disabledPrevMonth: boolean;
 }
 
 const CalendarHeader = ({
 	currentMonth,
 	onClickPrevMonth,
 	onClickNextMonth,
+	disabledPrevMonth,
 }: calendarHeaderType) => {
 	return (
 		<div css={headerLayoutStyle}>
-			<LeftArrowIcon onClick={onClickPrevMonth} />
+			{disabledPrevMonth ? <LeftArrowDisabledIcon /> : <LeftArrowIcon onClick={onClickPrevMonth} />}
 			<div css={headerBoxStyle}>
 				<h1>{format(currentMonth, "M")}월</h1>
 				<p>{format(currentMonth, "yyyy")}년</p>
