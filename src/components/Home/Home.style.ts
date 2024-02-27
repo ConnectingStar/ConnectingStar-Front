6d13b227e96a5a54770bb7fc659ea628dd15ca06
김영택 <kooduck168@gmail.com>
2024-02-27 16:22:16 +0900

    [TS-4] Fix: Header font style 수정, 잘 못 커밋된 모달 삭제, style 정리

--- src/components/Home/Home.style.ts
+++ src/components/Home/Home.style.ts
@@ -1,17 +1,9 @@
 import { css } from "@emotion/react";
 
 export const homeStyle = {
-	container: css`
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-		min-width: 100%;
-		background-color: white;
-	`,
 	innerWrapper: css`
 		display: flex;
 		flex-direction: column;
-		width: 100%;
 		padding: 0 1.5rem 0 1.5rem;
 		gap: 1.25rem;
 	`,
--- src/components/Home/Profile.tsx
+++ src/components/Home/Profile.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 
 import ProfileButtonIcon from "@/assets/icon/ic-homepage-to-mypage.svg?react";
+import StarImage from "@/assets/image/img-card-detail-star-button.png";
 
 import { profileStyle } from "@/components/Home/Profile.style";
 
@@ -10,7 +11,7 @@ function Profile() {
 		<section css={profileStyle.container}>
 			<div style={{ display: "flex" }} onClick={() => navigate("/mypage")}>
 				<div css={profileStyle.imageWrapper}>
-					<img src="" alt="" />
+					<img src={StarImage} alt="임시" />
 				</div>
 				<div css={profileStyle.names}>
 					<span className="identity">매일 성장하는</span>
