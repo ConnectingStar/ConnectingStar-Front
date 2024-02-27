06c1743266a142e758acc143ea101ec7e9e05e78
sikkzz <sling0623@gmail.com>
2024-02-27 17:56:37 +0900

    [TS-4] Refactor: profile, habit help component refactoring

--- src/components/Home/Profile.tsx
+++ src/components/Home/Profile.tsx
@@ -3,24 +3,21 @@ import { useNavigate } from "react-router-dom";
 import ProfileButtonIcon from "@/assets/icon/ic-homepage-to-mypage.svg?react";
 import StarImage from "@/assets/image/img-card-detail-star-button.png";
 
-import { profileStyle } from "@/components/Home/Profile.style";
+import { layoutStyle, profileBoxStyle, profileTextBoxStyle } from "@/components/Home/Profile.style";
 
 function Profile() {
 	const navigate = useNavigate();
+
 	return (
-		<section css={profileStyle.container}>
-			<div style={{ display: "flex" }} onClick={() => navigate("/mypage")}>
-				<div css={profileStyle.imageWrapper}>
-					<img src={StarImage} alt="임시" />
-				</div>
-				<div css={profileStyle.names}>
-					<span className="identity">매일 성장하는</span>
-					<span className="nickname">사용자닉네임</span>
+		<section css={layoutStyle}>
+			<div css={profileBoxStyle} onClick={() => navigate("/mypage")}>
+				<img src={StarImage} alt="user 프로필 이미지" />
+				<div css={profileTextBoxStyle}>
+					<span>매일 성장하는</span>
+					<span>사용자닉네임</span>
 				</div>
 			</div>
-			<div css={profileStyle.buttonWrapper} onClick={() => navigate("/habitgenerate")}>
-				<ProfileButtonIcon />
-			</div>
+			<ProfileButtonIcon onClick={() => navigate("/habitgenerate")} />
 		</section>
 	);
 }
