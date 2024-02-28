3ec7e4e1eecd20c79af585a38b003e6477105601
pizzaYami <cyj9407@naver.com>
2024-02-28 09:49:22 +0900

    [TS-25] Fix: progressbar 로직변경

--- src/components/Chat/Progressbar.style.ts
+++ src/components/Chat/Progressbar.style.ts
@@ -2,13 +2,13 @@ import { css } from "@emotion/react";
 
 import { theme } from "@/styles/theme";
 
-export const progressbarStyle = (percentage: number) => css`
+export const progressbarStyle = (calculateProgress: number) => css`
 	width: 100%;
 	height: 4px;
 	background-color: ${theme.color.button_disabled};
 	& > div {
 		height: 4px;
-		width: ${percentage}%;
+		width: ${calculateProgress}%;
 		background-color: ${theme.color.main_blue};
 	}
 `;
