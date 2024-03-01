0afb45bf26712bfa612389957a74f4c39499219e
khkh0109 <ist200120@gmail.com>
2024-03-02 02:51:53 +0900

    [TS-286] Refactor: StarCardDetailPage 버튼을 공통 버튼으로 교체
    - 기존 버튼 삭제 & 공통 버튼으로 교체
    - 교체 후 필요 없는 주석 및 스타일 삭제
    - 교체 후 레이아웃을 맞추기 위해 sectionStyle padding 수정

--- src/pages/StarCardDetailPage.tsx
+++ src/pages/StarCardDetailPage.tsx
@@ -1,7 +1,7 @@
 import { css } from "@emotion/react";
 
+import FooterBtn from "@/components/common/FooterBtn/FooterBtn";
 import Header from "@/components/common/Header/Header";
-import Button from "@/components/StarPage/StarCardDetail/Button";
 import CategoryLabel from "@/components/StarPage/StarCardDetail/CategoryLabel";
 import Img from "@/components/StarPage/StarCardDetail/Img/Img";
 import Story from "@/components/StarPage/StarCardDetail/Story";
@@ -19,7 +19,6 @@ const data: dataType = {
 	state: "have",
 };
 
-// TODO: Button 공통 컴포넌트 머지되면 교체 예정
 export default function StarCardDetailPage() {
 	return (
 		<>
@@ -32,21 +31,14 @@ export default function StarCardDetailPage() {
 				<Title tag="h1">캐릭터 이름</Title>
 				<CategoryLabel />
 				<Story />
-				<div css={buttonContainerStyle}>
-					<Button>{buttonState[data.state]}</Button>
-				</div>
 			</section>
+			<FooterBtn text={buttonState[data.state]} />
 		</>
 	);
 }
 
 export const sectionStyle = css`
 	width: 22.5rem;
-	padding: 1.25rem 1.5rem 1rem 1.5rem;
+	padding: 1.25rem 1.5rem 5.4375rem 1.5rem;
 	margin: 0 auto;
-`;
-
-// TODO: 공통 버튼 컴포넌트 추가되면 교체 예정
-export const buttonContainerStyle = css`
-	margin-top: 1rem;
 `;
