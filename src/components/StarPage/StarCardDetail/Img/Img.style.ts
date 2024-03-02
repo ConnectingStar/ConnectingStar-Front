629781741153631c8458295ffc920a7071bf66eb
khkh0109 <ist200120@gmail.com>
2024-03-02 17:37:34 +0900

    [TS-286] Refactor: Label 컴포넌트 삭제
    자주 사용하지 않아서 삭제함

--- src/components/StarPage/StarCardDetail/Img/Img.style.ts
+++ src/components/StarPage/StarCardDetail/Img/Img.style.ts
@@ -3,7 +3,7 @@ import { css } from "@emotion/react";
 import { theme } from "@/styles/theme";
 
 export const imgStyle = css`
-	height: 312px;
+	height: 19.5rem;
 	border-radius: 15px;
 	margin-bottom: 1.25rem;
 	position: relative;
@@ -19,4 +19,16 @@ export const imgStyle = css`
 	&.selected {
 		box-shadow: 0 0 0 5px ${theme.color.main_blue} inset;
 	}
+`;
+
+export const labelStyle = css`
+	width: 6.25rem;
+	padding: 1rem 0;
+	border-radius: 15px 0 15px 0;
+	position: absolute;
+	top: 0;
+	${theme.font.body_a_bold}
+	text-align: center;
+	color: #fff;
+	background-color: ${theme.color.main_deep_blue};
 `;
