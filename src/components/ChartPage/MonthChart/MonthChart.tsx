c1ce729bfa315d2a89d82aa5df048728584904fd
sikkzz <sling0623@gmail.com>
2024-03-04 04:27:41 +0900

    [TS-285] Design: month chart calendar header styling

--- src/components/ChartPage/MonthChart/MonthChart.tsx
+++ src/components/ChartPage/MonthChart/MonthChart.tsx
@@ -1,13 +1,36 @@
+import { useState, useCallback } from "react";
+
+import { addMonths, subMonths } from "date-fns";
+
+import CalendarHeader from "@/components/ChartPage/MonthChart/CalendarHeader";
+
 import { layoutStyle, boxStyle } from "@/components/ChartPage/MonthChart/MonthChart.style";
 
 const MonthChart = () => {
+	const [currentMonth, setCurrentMonth] = useState(new Date());
+	const [nowMonth] = useState(new Date());
+
+	const handlePrevMonth = useCallback(() => {
+		setCurrentMonth(subMonths(currentMonth, 1));
+	}, [currentMonth]);
+
+	const handleNextMonth = useCallback(() => {
+		if (currentMonth >= nowMonth) {
+			setCurrentMonth(nowMonth);
+		} else {
+			setCurrentMonth(addMonths(currentMonth, 1));
+		}
+	}, [currentMonth]);
+
 	return (
 		<div css={layoutStyle}>
 			<div css={boxStyle}>
-				<h2>일주일중</h2>
-				<h3>
-					<span>5일</span> 해냈어요!
-				</h3>
+				<CalendarHeader
+					currentMonth={currentMonth}
+					onClickNextMonth={handleNextMonth}
+					onClickPrevMonth={handlePrevMonth}
+					disabledNextMonth={currentMonth >= nowMonth}
+				/>
 			</div>
 		</div>
 	);
