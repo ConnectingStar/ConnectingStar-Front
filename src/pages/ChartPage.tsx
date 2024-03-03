cb6673072ecadca580e479b53ed42f51f987edbd
sikkzz <sling0623@gmail.com>
2024-03-04 04:00:00 +0900

    [TS-285] Feat: create tab param

--- src/pages/ChartPage.tsx
+++ src/pages/ChartPage.tsx
@@ -1,3 +1,7 @@
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+
+import MonthChart from "@/components/ChartPage/MonthChart/MonthChart";
 import Tab from "@/components/ChartPage/Tab/Tab";
 import TotalInfo from "@/components/ChartPage/TotalInfo/TotalInfo";
 import WeekChart from "@/components/ChartPage/WeekChart/WeekChart";
@@ -5,7 +9,21 @@ import ButtonCarousel from "@/components/common/ButtonCarousel/ButtonCarousel";
 import Gnb from "@/components/common/Gnb/Gnb";
 import Header from "@/components/common/Header/Header";
 
+import { TAB_KEY, TAB_PARAM } from "@/constants/tabConstants";
+
 const ChartPage = () => {
+	const [searchParams, setSearchParams] = useSearchParams();
+
+	useEffect(() => {
+		if (
+			searchParams.size === 0 ||
+			(searchParams.get(TAB_KEY) !== TAB_PARAM.WEEK &&
+				searchParams.get(TAB_KEY) !== TAB_PARAM.MONTH)
+		) {
+			setSearchParams(`${TAB_KEY}=${TAB_PARAM.WEEK}`);
+		}
+	}, [searchParams]);
+
 	return (
 		<>
 			<Header>
@@ -13,8 +31,9 @@ const ChartPage = () => {
 			</Header>
 			<ButtonCarousel />
 			<TotalInfo />
-			<Tab />
-			<WeekChart />
+			<Tab searchParams={searchParams} setSearchParams={setSearchParams} />
+			{searchParams.get(TAB_KEY) === TAB_PARAM.WEEK ? <WeekChart /> : <MonthChart />}
+
 			<Gnb />
 		</>
 	);
