32cac2cd025ead9b9924ba0df3c5fa18070a9821
pizzaYami <cyj9407@naver.com>
2024-03-04 18:25:24 +0900

    [TS-53] Feat: modal과 연결

--- src/pages/CreateAccountPage.tsx
+++ src/pages/CreateAccountPage.tsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
+
 import ArrowDown from "@/assets/icon/arrow-down.svg?react";
 
+import FooterBtn from "@/components/common/FooterBtn/FooterBtn";
 import Header from "@/components/common/Header/Header";
+import ChangeNicknameModal from "@/components/common/Modal/CommonModal/ChangeNicknameModal";
+import SelectAgeModal from "@/components/common/Modal/CommonModal/SelectAgeModal";
+import SelectGenderModal from "@/components/common/Modal/CommonModal/SelectGenderModal/SelectGenderModal";
+
+import { useAppDispatch, useAppSelector } from "@/api/hooks";
+import { openModal } from "@/api/modal/modalSlice";
+
+import { modalType } from "@/constants/modalConstants";
 
 import { createAccountStyle } from "@/pages/CreateAccountPage.style";
 
@@ -23,6 +34,14 @@ const accountInputData = [
 ];
 
 export default function CreateAccountPage() {
+	const dispatch = useAppDispatch();
+
+	const { modal } = useAppSelector((state) => state.modal);
+
+	const [nickName, setNickName] = useState<string>("");
+	const [gender, setGender] = useState<string>("");
+	const [age, setAge] = useState<string>("");
+
 	return (
 		<>
 			<Header>
@@ -31,12 +50,20 @@ export default function CreateAccountPage() {
 			<div css={createAccountStyle.container}>
 				<h1>내 정보 입력을 완료해 주세요</h1>
 				<ul css={createAccountStyle.wrap}>
-					{accountInputData.map((item) => {
+					{accountInputData.map((item, index) => {
 						return (
-							<li>
+							<li key={item.title}>
 								<h2>{item.title}</h2>
-								<div>
-									{item.content}
+								<div
+									onClick={() => {
+										index === 0 && dispatch(openModal(modalType.CHANGE_NICKNAME));
+										index === 1 && dispatch(openModal(modalType.SELECT_GENDER));
+										index === 2 && dispatch(openModal(modalType.SELECT_AGE));
+									}}
+								>
+									{index === 0 && (nickName ? nickName : item.content)}
+									{index === 1 && (gender ? gender : item.content)}
+									{index === 2 && (age ? age : item.content)}
 									{item.isPopUpModal && <ArrowDown />}
 								</div>
 							</li>
@@ -44,6 +71,11 @@ export default function CreateAccountPage() {
 					})}
 				</ul>
 			</div>
+			<FooterBtn text="다음" />
+
+			{modal === modalType.CHANGE_NICKNAME && <ChangeNicknameModal changeNickname={setNickName} />}
+			{modal === modalType.SELECT_GENDER && <SelectGenderModal changeGender={setGender} />}
+			{modal === modalType.SELECT_AGE && <SelectAgeModal changeAge={setAge} />}
 		</>
 	);
 }
