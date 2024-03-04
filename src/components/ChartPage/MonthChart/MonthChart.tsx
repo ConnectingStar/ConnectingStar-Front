91611e7d2db41fa061705d197f26e61ab53c5e59
sikkzz <sling0623@gmail.com>
2024-03-04 17:33:19 +0900

    [TS-285] Design: month chart section title styling

--- src/components/ChartPage/MonthChart/MonthChart.tsx
+++ src/components/ChartPage/MonthChart/MonthChart.tsx
@@ -9,9 +9,12 @@ import { habitIconData } from "@/constants/myPageConstants";
 
 import {
 	layoutStyle,
-	boxStyle,
+	innerBoxStyle,
+	calendarSectionStyle,
 	calendarBoxStyle,
 	iconListBoxStyle,
+	chartSectionStyle,
+	chartBoxStyle,
 } from "@/components/ChartPage/MonthChart/MonthChart.style";
 
 const MonthChart = () => {
@@ -31,36 +34,49 @@ const MonthChart = () => {
 	}, [currentMonth]);
 
 	return (
-		<div css={layoutStyle}>
-			<div css={boxStyle}>
-				<CalendarHeader
-					currentMonth={currentMonth}
-					onClickNextMonth={handleNextMonth}
-					onClickPrevMonth={handlePrevMonth}
-					disabledNextMonth={currentMonth >= nowMonth}
-				/>
+		<main css={layoutStyle}>
+			<div css={innerBoxStyle}>
+				<section css={calendarSectionStyle}>
+					<CalendarHeader
+						currentMonth={currentMonth}
+						onClickNextMonth={handleNextMonth}
+						onClickPrevMonth={handlePrevMonth}
+						disabledNextMonth={currentMonth >= nowMonth}
+					/>
 
-				<h2>
-					<span>27일</span> 해냈어요!
-				</h2>
+					<h2>
+						<span>27일</span> 해냈어요!
+					</h2>
+					<div css={calendarBoxStyle}>
+						<Calendar currentMonth={currentMonth} />
 
-				<div css={calendarBoxStyle}>
-					<Calendar currentMonth={currentMonth} />
+						<div className="divider" />
 
-					<div className="divider" />
-
-					{/* api 연결 후 아이콘 데이터 변경 예정 */}
-					<div css={iconListBoxStyle}>
-						{habitIconData.map((data) => (
-							<div key={data.id} className="iconBox">
-								{data.icon}
-								<p>3</p>
-							</div>
-						))}
+						{/* api 연결 후 아이콘 데이터 변경 예정 */}
+						<div css={iconListBoxStyle}>
+							{habitIconData.map((data) => (
+								<div key={data.id} className="iconBox">
+									{data.icon}
+									<p>3</p>
+								</div>
+							))}
+						</div>
 					</div>
-				</div>
+				</section>
+
+				<section css={chartSectionStyle}>
+					<p>{currentMonth.getMonth() + 1}월</p>
+					<h1>
+						<span>평균 6 페이지,</span>
+						<br />
+						<span>누적 120 페이지</span>
+						<br />
+						해냈어요!
+					</h1>
+					<div css={chartBoxStyle}></div>
+				</section>
 			</div>
-		</div>
+		</main>
 	);
 };
 
