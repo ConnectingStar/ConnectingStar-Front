15c54b341b1c007d96701448516c18e184f2ccd9
pizzaYami <cyj9407@naver.com>
2024-03-04 19:03:57 +0900

    [TS-53] Design: px -> rem

--- src/pages/CreateAccountPage.style.ts
+++ src/pages/CreateAccountPage.style.ts
@@ -6,29 +6,29 @@ export const createAccountStyle = {
 	container: css`
 		width: 22.5rem;
 		margin: 0 auto;
-		padding: 20px 24px;
+		padding: 1.25rem 1.5rem;
 
 		& > h1 {
 			${theme.font.head_a}
-			margin-bottom: 40px;
+			margin-bottom: 2.5rem;
 		}
 	`,
 	wrap: css`
 		width: 100%;
 		& > li {
 			width: 100%;
-			height: 87px;
-			margin-bottom: 20px;
+			height: 5.438rem;
+			margin-bottom: 1.25rem;
 			& > h2 {
 				${theme.font.head_c}
 				color: ${theme.color.font_gray};
-				margin-bottom: 12px;
+				margin-bottom: 0.75rem;
 			}
 			& > div {
 				display: flex;
 				justify-content: space-between;
 				width: 100%;
-				height: 55px;
+				height: 3.438rem;
 				background-color: ${theme.color.bg};
 				color: ${theme.color.button_deactivated};
 				${theme.font.body_a};
