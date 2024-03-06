da7ed2782a2839fac0136ce76715b54cbbeea12b
김영택 <kooduck168@gmail.com>
2024-03-06 20:28:08 +0900

    [TS-30] Feat: HabitGeneratePage  Header Title과 layoutStyle 작업

--- src/pages/HabitGeneratePage.tsx
+++ src/pages/HabitGeneratePage.tsx
@@ -1,9 +1,16 @@
 import Header from "@/components/common/Header/Header";
+import HabitGenerate from "@/components/Home/HabitGenerate/HabitGenerate";
 
-function HabitGeneraatePage() {
-	<>
-		<Header></Header>
-	</>;
+function HabitGeneratePage() {
+	return (
+		<>
+			<Header>
+				<Header.PrevButton />
+				<Header.Title>습관 약속 만들기</Header.Title>
+			</Header>
+			<HabitGenerate />
+		</>
+	);
 }
 
-export default HabitGeneraatePage;
+export default HabitGeneratePage;
