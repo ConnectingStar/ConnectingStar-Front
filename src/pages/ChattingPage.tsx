ce493dcf44ee1354cb6d77aec4d23c4df80670c5
pizzaYami <cyj9407@naver.com>
2024-03-06 20:14:33 +0900

    [TS-25] Feat: chattingPage 2차완성

--- src/pages/ChattingPage.tsx
+++ src/pages/ChattingPage.tsx
@@ -1,29 +1,23 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
 import ChattingMessage from "@/components/Chat/ChattingMessage";
 import Progressbar from "@/components/Chat/Progressbar";
 import Header from "@/components/common/Header/Header";
 
-import { chatData } from "@/constants/chatData";
+import { RootState } from "@/api/store";
+
+import { createChatData } from "@/constants/chatData";
 
 import { chattingPageStyle } from "@/pages/ChattingPage.style";
-
-// TODO: 나중에 초기화
-export const userDataFrame = {
-	nickName: "닉네임",
-	habit: "습관",
-	identity: "정체성",
-	time: "오후 10:00",
-	location: "집 앞 산책로",
-	action: "5장",
-	alert1: "오후 09:50",
-	alert2: "오후 10:10",
-};
 
 function ChattingPage() {
 	const [progress, setProgress] = useState(0);
 
-	//TODO: reply 변경하기
+	const userData = useSelector((state: RootState) => state.userData);
+	const chatData = createChatData(userData);
+	const isbuttonHeiger = chatData[progress].replyBtnMessage.length > 1;
+
 	return (
 		<div css={chattingPageStyle.container}>
 			<div css={chattingPageStyle.header}>
@@ -32,12 +26,11 @@ function ChattingPage() {
 				</Header>
 				<Progressbar currentClicks={progress} totalClicksNeeded={11} />
 			</div>
-			<div css={chattingPageStyle.chattingWrap}>
-				{chatData.slice(0, progress + 1).map((userData) => (
+			<div css={chattingPageStyle.chattingWrap(isbuttonHeiger)}>
+				{chatData.slice(0, progress + 1).map((chatData) => (
 					<ChattingMessage
-						key={userData.id}
-						userData={userData}
-						reply={userDataFrame.habit}
+						key={chatData.id}
+						chatData={chatData}
 						setProgress={setProgress}
 					></ChattingMessage>
 				))}
