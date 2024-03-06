fd2fc8227bf16e917e41c756bda9f352d25512dd
pizzaYami <cyj9407@naver.com>
2024-03-06 20:33:59 +0900

    [TS-144] Remove: route chat 두개 있던거 제거

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,7 @@ const App = () => {
 				<Route path="/myinfo" element={<MyInfoPage />} />
 				<Route path="/signup" element={<SignUp />} />
 				<Route path="/oauthsignup" element={<OauthSignUp />} />
+				<Route path="/star/:id" element={<StarCardDetailPage />} />
 				<Route path="/star-trace" element={<MyStarTracePage />} />
 				<Route path="/habit-history" element={<MyHabitPage />} />
 				<Route path="/notification-setting" element={<NotificationSettingPage />} />
