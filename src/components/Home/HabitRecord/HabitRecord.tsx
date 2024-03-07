4aa53619d481a1ad1a973b145bdb359f2e38818f
김영택 <kooduck168@gmail.com>
2024-03-07 21:51:06 +0900

    [TS-28] Fix: starPrizeModal path 변경, style 통합

--- src/components/Home/HabitRecord/HabitRecord.tsx
+++ src/components/Home/HabitRecord/HabitRecord.tsx
@@ -3,7 +3,7 @@ import { useState, ChangeEvent, useEffect } from "react";
 import ExclamationMarkIcon from "@/assets/icon/ic-exclamation-mark.svg?react";
 
 import FooterBtn from "@/components/common/FooterBtn/FooterBtn";
-import StarPrizeModal from "@/components/Home/HabitRecord/StarPrizeModal/StarPrizeModal";
+import StarPrizeModal from "@/components/Home/Landing/Modal/StarPrizeModal";
 
 import { useAppDispatch, useAppSelector } from "@/api/hooks";
 import { openModal } from "@/api/modal/modalSlice";
