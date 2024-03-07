c152a552ba0e3b4c0bfb0dda849c753cabdaaedc
김영택 <kooduck168@gmail.com>
2024-03-07 21:51:06 +0900

    [TS-28] Fix: starPrizeModal path 변경, style 통합

--- src/components/Home/Landing/Modal/StarPrizeModal.tsx
+++ src/components/Home/Landing/Modal/StarPrizeModal.tsx
@@ -1,3 +1,5 @@
+import { css } from "@emotion/react";
+
 import StarImage from "@/assets/image/card-detail-star.png";
 
 import FooterBtn from "@/components/common/FooterBtn/FooterBtn";
@@ -8,11 +10,7 @@ import { closeModal } from "@/api/modal/modalSlice";
 
 import { prizeCommentsArray } from "@/constants/habitRecordConstants";
 
-import {
-	imageWrapperStyle,
-	prizeCommentStyle,
-	containerStyle,
-} from "@/components/Home/HabitRecord/StarPrizeModal/StarPrizeModal.style";
+import { theme } from "@/styles/theme";
 
 function StarPrizeModal() {
 	const dispatch = useAppDispatch();
@@ -42,3 +40,41 @@ function StarPrizeModal() {
 }
 
 export default StarPrizeModal;
+
+export const containerStyle = css`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	gap: 20px;
+	width: 22.5rem;
+	height: 100vh;
+`;
+
+export const imageWrapperStyle = css`
+	border-radius: 15px;
+	img {
+		width: 13rem;
+		height: 13rem;
+		object-fit: contain;
+	}
+`;
+
+export const prizeCommentStyle = css`
+	white-space: pre-wrap;
+	text-align: center;
+	& > :first-of-type {
+		${theme.font.head_a}
+		& > .blue {
+			color: ${theme.color.main_blue};
+		}
+		& > .yellow {
+			color: ${theme.color.main_yellow};
+		}
+	}
+
+	& > :nth-of-type(2) {
+		${theme.font.body_a};
+		color: white;
+	}
+`;
