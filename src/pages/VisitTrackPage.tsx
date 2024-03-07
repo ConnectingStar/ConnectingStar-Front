519ef62f7624367d157b111ab3796c80cacffe62
pizzaYami <cyj9407@naver.com>
2024-03-07 13:59:10 +0900

    [TS-53] Fix: PR수정사항
    FooterBtn 기존과 다른거 수정
    의미없는 css제거
    CreateAccountPage 리팩토링

--- src/pages/VisitTrackPage.tsx
+++ src/pages/VisitTrackPage.tsx
@@ -1,6 +1,6 @@
 import Header from "@/components/common/Header/Header";
 
-import { visitTrackStyle } from "./VisitTrackPage.style";
+import { visitTrackStyle } from "@/pages/VisitTrackPage.style";
 
 function VisitTrackPage() {
 	const discoveryRouteText = ["앱 스토어", "지인 추천", "SNS 광고", "검색", "기타"];
@@ -8,7 +8,7 @@ function VisitTrackPage() {
 	return (
 		<>
 			<Header>
-				<Header.PrevButton></Header.PrevButton>
+				<Header.PrevButton />
 			</Header>
 			<div css={visitTrackStyle}>
 				<h1>어떤 경로를 통해 오셨나요?</h1>
