2f76ff3d612683430fb625d812974c197793820c
sikkzz <sling0623@gmail.com>
2024-03-10 19:34:53 +0900

    [TS-33] Refactor: unused style delete

--- src/components/common/SelectReason/SelectReasonModal/SelectReasonModal.tsx
+++ src/components/common/SelectReason/SelectReasonModal/SelectReasonModal.tsx
@@ -53,8 +53,6 @@ const SelectReasonModal = ({
 export default SelectReasonModal;
 
 export const layoutStyle = css`
-	width: 100%;
-	height: 25.4375rem;
 	padding: 1.125rem 1.5rem;
 	border-radius: 15px 15px 0 0;
 	color: ${theme.color.font_black};
