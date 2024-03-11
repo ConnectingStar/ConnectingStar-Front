4566e050c890efefad5a02139d2835f1faccd0d2
khkh0109 <ist200120@gmail.com>
2024-03-11 19:33:48 +0900

    [TS-276] Refactor: StarCount -> StarInfo로 이름 변경 및 '캐릭터 이름' 링크 추가
    캐릭터 이름 링크와 보유 별 개수를 한 컴포넌트로 묶음
    이에따라 컴포넌트 이름도 StarCount에서 StarInfo로 변경함
    
    별자리에 관한 정보가 있는 컴포넌트라 StarInfo로 작명함

---
+++ src/components/StarPage/StarMain/StarInfo.tsx
@@ -0,0 +1,59 @@
+import { Link } from "react-router-dom";
+
+import { css } from "@emotion/react";
+
+import ArrowRightIcon from "@/assets/icon/ic-right-arrow-with-shadow.svg?react";
+import star from "@/assets/image/img-3d-star.png";
+
+import { theme } from "@/styles/theme";
+
+import { getOutlineTextStyle } from "@/components/StarPage/StarMain/getOutlineText.style";
+
+// TODO: 별 개수(starCount)는 API 연결 후 수정 예정
+interface StarInfoProps {
+	starCount: number;
+	starCardId: number;
+}
+
+export default function StarInfo({ starCount, starCardId }: StarInfoProps) {
+	return (
+		<>
+			<div css={starCountStyle}>
+				<img src={star} alt="3D 별" />
+				<p>{starCount}</p>
+			</div>
+			<Link to={`/star-card/${starCardId}`} css={characterLinkStyle}>
+				<span css={getOutlineTextStyle("#7400cf", "head_a")} data-text="캐릭터 이름">
+					캐릭터 이름
+				</span>
+				<ArrowRightIcon />
+			</Link>
+		</>
+	);
+}
+
+const starCountStyle = css`
+	width: fit-content;
+	margin: 0 0 1.875rem 0.875rem;
+	position: relative;
+
+	img {
+		width: 3.125rem;
+		position: absolute;
+		top: -0.5rem;
+		left: -0.875rem;
+	}
+
+	p {
+		padding: 0.1875rem 1.625rem 0.25rem 2.8125rem;
+		border-radius: 15px;
+		${theme.font.body_a_bold}
+		color: #fff;
+		background-color: #38567880; // main_deep_blue + 투명도 50%
+	}
+`;
+
+const characterLinkStyle = css`
+	display: flex;
+	align-items: center;
+`;
