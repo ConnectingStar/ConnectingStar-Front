40319f5d857f37347d1b1ab6e589696b0aad6b58
khkh0109 <ist200120@gmail.com>
2024-03-11 19:33:48 +0900

    [TS-276] Refactor: StarCount -> StarInfo로 이름 변경 및 '캐릭터 이름' 링크 추가
    캐릭터 이름 링크와 보유 별 개수를 한 컴포넌트로 묶음
    이에따라 컴포넌트 이름도 StarCount에서 StarInfo로 변경함
    
    별자리에 관한 정보가 있는 컴포넌트라 StarInfo로 작명함

--- src/pages/StarMainPage.tsx
+++ src/pages/StarMainPage.tsx
@@ -3,14 +3,14 @@ import { css } from "@emotion/react";
 import Gnb from "@/components/common/Gnb/Gnb";
 import StarButton from "@/components/StarPage/StarMain/StarButton";
 import StarCardLink from "@/components/StarPage/StarMain/StarCardLink";
-import StarCount from "@/components/StarPage/StarMain/StarCount";
+import StarInfo from "@/components/StarPage/StarMain/StarInfo";
 
 import { theme } from "@/styles/theme";
 
 export default function StarMainPage() {
 	return (
 		<div css={containerStyle}>
-			<StarCount starCount={114} />
+			<StarInfo starCount={114} starCardId={1} />
 			<div className="wrapper">
 				<StarButton />
 				<StarCardLink />
