48e1fdb95fde4c8855f2a19c788caa4afbb5f1b3
sikkzz <sling0623@gmail.com>
2024-03-12 20:49:59 +0900

    [TS-290] Refactor: gnb style refactoring

--- src/components/common/Gnb/Gnb.style.ts
+++ src/components/common/Gnb/Gnb.style.ts
@@ -1,9 +1,13 @@
 import { css } from "@emotion/react";
 
 export const layoutStyle = css`
+	position: fixed;
+	bottom: 0;
+	left: 50%;
+	transform: translateX(-50%);
 	width: 22.5rem;
-	margin: 0 auto;
 	padding: 0.875rem 2.6875rem;
+	background-color: #fff;
 
 	& > ul {
 		display: flex;
