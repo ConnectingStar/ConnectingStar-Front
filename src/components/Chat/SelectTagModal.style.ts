8b48d97453472398ca2d1b390942903b10854d4c
sikkzz <sling0623@gmail.com>
2024-03-13 18:20:35 +0900

    [TS-144] Fix: input focus시 아웃라인과 배경색 변경되는 현상

--- src/components/Chat/SelectTagModal.style.ts
+++ src/components/Chat/SelectTagModal.style.ts
@@ -56,10 +56,5 @@ export const selectTagModalStyle = {
 			background-color: ${theme.color.main_blue};
 			color: white;
 		}
-
-		.selected {
-			background-color: ${theme.color.main_blue};
-			color: white;
-		}
 	`,
 };
