2c92a63021f5702b80369a3075b3ff87562493e9
pizzaYami <cyj9407@naver.com>
2024-03-13 18:52:13 +0900

    [TS-53] Fix: 이모티콘 막고나서 nickname.length가 남는현상

--- src/components/common/Modal/CommonModal/ChangeNicknameModal.tsx
+++ src/components/common/Modal/CommonModal/ChangeNicknameModal.tsx
@@ -12,9 +12,10 @@ import { closeModal } from "@/api/modal/modalSlice";
 
 import { theme } from "@/styles/theme";
 
-// 이모지 판별을 위한 정규식
-const emojiRegex =
-	/(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
+function isValidStringIncludingHangulJamo(s) {
+	const regex = /^[가-힣\u3131-\u314E\u314F-\u3163a-zA-Z0-9!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]+$/;
+	return regex.test(s);
+}
 
 const ChangeNicknameModal = ({
 	changeNickname,
@@ -26,15 +27,12 @@ const ChangeNicknameModal = ({
 	const [nickname, setNickname] = useState("");
 
 	const handleNicknameChange = (e: ChangeEvent<HTMLInputElement>) => {
-		let input = e.target.value;
+		const input = e.target.value;
 
-		// 띄어쓰기 작성 막기
-		input = input.replace(/\s+/g, "");
-		// 이모지 작성 막기
-		input = input.replace(emojiRegex, "");
-
-		// 8자이상 입력 막기
-		setNickname(input);
+		if (isValidStringIncludingHangulJamo(input) || input === "") {
+			setNickname(input);
+			a;
+		}
 	};
 
 	const handleChangeInput = () => {
@@ -63,6 +61,7 @@ const ChangeNicknameModal = ({
 				leftText="취소"
 				handleBtnClick={handleChangeInput}
 				handleLeftBtnClick={() => dispatch(closeModal())}
+				disabled={nickname.trim().length === 0}
 				isSquare
 			/>
 		</Modal>
