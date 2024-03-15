e8d57309c333303941bc3948951294d19259ee14
pizzaYami <cyj9407@naver.com>
2024-03-15 17:36:24 +0900

    [TS-61] Feat: SelectTagModal

--- src/pages/ChattingPage.tsx
+++ src/pages/ChattingPage.tsx
@@ -1,9 +1,26 @@
-import LocationModal from "@/components/Chatting/LocationModal";
+// import LocationModal from "@/components/Chatting/LocationModal";
+// import SelectTagModal from "@/components/Chatting/SelectTagModal";
+
+// const habitTags = [
+// 	"러닝하기",
+// 	"헬스하기",
+// 	"산책하기",
+// 	"명상하기",
+// 	"기도하기",
+// 	"자기확언",
+// 	"책 읽기",
+// 	"신문보기",
+// 	"공부하기",
+// 	"블로깅",
+// 	"일기작성",
+// 	"소비기록",
+// ];
 
 function ChattingPage() {
 	return (
 		<div>
-			<LocationModal />
+			{/* <LocationModal /> */}
+			{/* <SelectTagModal title="어떤 습관을 만들어 볼까요?" tags={habitTags} /> */}
 		</div>
 	);
 }
