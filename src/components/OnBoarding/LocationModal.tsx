f6d4005bf544ea73a54f8083df5fee5f70a92b7e
pizzaYami <cyj9407@naver.com>
2024-03-15 15:18:06 +0900

    [TS-64] Feat: LocationModal 완성

---
+++ src/components/OnBoarding/LocationModal.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+import CheckIcon from "@/assets/icon/ic-check-blue.svg?react";
+
+import FooterBtn from "@/components/common/FooterBtn/FooterBtn";
+import Header from "@/components/common/Header/Header";
+import {
+	container,
+	wrap,
+	locationListStyle,
+	locationInputStyle,
+} from "@/components/OnBoarding/LocationModalStyle";
+
+import { locationModalData } from "@/constants/locationModalConstants";
+
+function LocationModal() {
+	const [location, setLocation] = useState("");
+	const [isFocus, setIsFocus] = useState(false);
+
+	return (
+		<div css={container}>
+			<Header>
+				<Header.CloseButton></Header.CloseButton>
+			</Header>
+			<div css={wrap}>
+				<h1>장소를 입력해 주세요</h1>
+				<ul css={locationListStyle}>
+					<p>예시</p>
+					{locationModalData.map(({ id, text }) => (
+						<li key={id}>
+							<CheckIcon />
+							{text}
+						</li>
+					))}
+				</ul>
+				<input
+					css={locationInputStyle}
+					type="text"
+					placeholder="직접입력"
+					onFocus={() => setIsFocus(true)}
+					onBlur={() => setIsFocus(false)}
+					onChange={(e) => setLocation(e.target.value)}
+				/>
+				{/* TODO: handleBtnClick로 데이터 전달 */}
+				{isFocus ? (
+					<FooterBtn
+						text="확인"
+						isSquare
+						isTransparent
+						disabled={!location}
+						handleBtnClick={() => {
+							setIsFocus(false);
+						}}
+					/>
+				) : (
+					<FooterBtn text="확인" disabled={!location} isTransparent />
+				)}
+			</div>
+		</div>
+	);
+}
+
+export default LocationModal;
---
+++ src/components/OnBoarding/LocationModalStyle.ts
@@ -0,0 +1,62 @@
+import { css } from "@emotion/react";
+
+import { theme } from "@/styles/theme";
+
+export const container = css`
+	position: fixed;
+	top: 0;
+	width: 100%;
+	height: 100vh;
+	background-color: white;
+`;
+export const wrap = css`
+	width: 22.5rem;
+	padding: 1.25rem 1.5rem;
+	margin: 0 auto;
+	& > h1 {
+		margin-bottom: 2.5rem;
+		${theme.font.head_a}
+	}
+`;
+
+export const locationListStyle = css`
+	display: flex;
+	flex-direction: column;
+	margin-bottom: 2.5rem;
+	gap: 0.375rem;
+	& > p {
+		margin-bottom: 0.375rem;
+		${theme.font.head_c};
+		color: ${theme.color.font_gray};
+	}
+	& > li {
+		display: flex;
+		gap: 0.75rem;
+		${theme.font.body_a};
+	}
+`;
+
+export const locationInputStyle = css`
+	display: flex;
+	align-items: center;
+	width: 19.5rem;
+	height: 3.438rem;
+	border: none;
+	border-radius: 15px;
+	padding: 15px;
+	${theme.font.body_a};
+	background-color: ${theme.color.bg};
+	color: black;
+
+	&::placeholder {
+		color: ${theme.color.button_deactivated};
+	}
+
+	&:focus {
+		position: fixed;
+		bottom: 4.688rem;
+		outline: none;
+		background-color: ${theme.color.bg};
+		color: black;
+	}
+`;
