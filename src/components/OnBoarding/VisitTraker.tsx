837189c9093d23c5662f338ac00ec683d700d47d
pizzaYami <cyj9407@naver.com>
2024-03-16 11:37:42 +0900

    [TS-54] Feat: VisitTraker

---
+++ src/components/OnBoarding/VisitTraker.tsx
@@ -0,0 +1,50 @@
+import { css } from "@emotion/react";
+
+import Header from "@/components/common/Header/Header";
+
+import { theme } from "@/styles/theme";
+
+function VisitTraker() {
+	const discoveryRouteText = ["앱 스토어", "지인 추천", "SNS 광고", "검색", "기타"];
+
+	return (
+		<>
+			<Header>
+				<Header.PrevButton />
+			</Header>
+			<div css={visitTrackStyle}>
+				<h1>어떤 경로를 통해 오셨나요?</h1>
+				<ul>
+					{discoveryRouteText.map((item) => (
+						<li>
+							<button>{item}</button>
+						</li>
+					))}
+				</ul>
+			</div>
+		</>
+	);
+}
+
+export default VisitTraker;
+
+export const visitTrackStyle = css`
+	width: 22.5rem;
+	margin: 0 auto;
+	padding: 1.25rem 1.5rem;
+	& > h1 {
+		${theme.font.head_a}
+		margin-bottom: 2.5rem;
+	}
+	button {
+		width: 100%;
+		height: 3.438rem;
+		border: 2px solid ${theme.color.line};
+		border-radius: 15px;
+		margin-bottom: 0.375rem;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		${theme.font.button_big}
+	}
+`;
