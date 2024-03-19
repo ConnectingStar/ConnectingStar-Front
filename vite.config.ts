f4cd1274ca66a6679e308d390f859ee182cbf64e
sikkzz <sling0623@gmail.com>
2024-03-19 18:25:25 +0900

    [TS-292] chore: pwa setting, icon add

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import { VitePWA } from "vite-plugin-pwa";
+
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
@@ -5,5 +7,41 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), svgr(), tsconfigPaths()],
+	plugins: [
+		react(),
+		svgr(),
+		tsconfigPaths(),
+		VitePWA({
+			registerType: "autoUpdate",
+			devOptions: {
+				enabled: true,
+			},
+			manifest: {
+				icons: [
+					{
+						src: "/icon-192.png",
+						type: "image/png",
+						sizes: "192x192",
+					},
+					{
+						src: "/icon-192-maskable.png",
+						type: "image/png",
+						sizes: "192x192",
+						purpose: "maskable",
+					},
+					{
+						src: "/icon-512.png",
+						type: "image/png",
+						sizes: "512x512",
+					},
+					{
+						src: "/icon-512-maskable.png",
+						type: "image/png",
+						sizes: "512x512",
+						purpose: "maskable",
+					},
+				],
+			},
+		}),
+	],
 });
