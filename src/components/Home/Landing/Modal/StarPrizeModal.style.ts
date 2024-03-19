bacb8d27fd18d7d0818bbc5c8a8e3cb284ba7d4f
김영택 <kooduck168@gmail.com>
2024-03-19 19:58:27 +0900

    [TS-28] Fix: StarPrizeModal style 분리

---
+++ src/components/Home/Landing/Modal/StarPrizeModal.style.ts
@@ -0,0 +1,42 @@
+import { css } from "@emotion/react";
+
+import { theme } from "@/styles/theme";
+
+export const containerStyle = css`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	gap: 20px;
+	width: 22.5rem;
+	height: 100vh;
+`;
+
+export const imageWrapperStyle = css`
+	border-radius: 15px;
+	img {
+		width: 13rem;
+		height: 13rem;
+		object-fit: contain;
+	}
+`;
+
+export const prizeCommentStyle = css`
+	white-space: pre-wrap;
+	text-align: center;
+
+	& > :first-of-type {
+		${theme.font.head_a};
+		margin-bottom: 0.5rem;
+		color: ${theme.color.main_blue};
+
+		.yellow {
+			color: ${theme.color.main_yellow};
+		}
+	}
+
+	& > :nth-of-type(2) {
+		${theme.font.body_a};
+		color: white;
+	}
+`;
--- src/components/Home/Landing/Modal/StarPrizeModal.tsx
+++ src/components/Home/Landing/Modal/StarPrizeModal.tsx
@@ -1,5 +1,3 @@
-import { css } from "@emotion/react";
-
 import StarImage from "@/assets/image/card-detail-star.png";
 
 import FooterBtn from "@/components/common/FooterBtn/FooterBtn";
@@ -10,7 +8,11 @@ import { closeModal } from "@/api/modal/modalSlice";
 
 import { prizeCommentsArray } from "@/constants/habitRecordConstants";
 
-import { theme } from "@/styles/theme";
+import {
+	containerStyle,
+	imageWrapperStyle,
+	prizeCommentStyle,
+} from "@/components/Home/Landing/Modal/StarPrizeModal.style";
 
 function StarPrizeModal() {
 	const dispatch = useAppDispatch();
@@ -40,41 +42,3 @@ function StarPrizeModal() {
 }
 
 export default StarPrizeModal;
-
-export const containerStyle = css`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	gap: 20px;
-	width: 22.5rem;
-	height: 100vh;
-`;
-
-export const imageWrapperStyle = css`
-	border-radius: 15px;
-	img {
-		width: 13rem;
-		height: 13rem;
-		object-fit: contain;
-	}
-`;
-
-export const prizeCommentStyle = css`
-	white-space: pre-wrap;
-	text-align: center;
-
-	& > :first-of-type {
-		${theme.font.head_a};
-		margin-bottom: 0.5rem;
-		color: ${theme.color.main_blue};
-		.yellow {
-			color: ${theme.color.main_yellow};
-		}
-	}
-
-	& > :nth-of-type(2) {
-		${theme.font.body_a};
-		color: white;
-	}
-`;
