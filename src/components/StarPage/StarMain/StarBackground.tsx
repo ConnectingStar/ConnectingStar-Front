c5e611ed232449163e0c1039f8f58aac9050a614
khkh0109 <ist200120@gmail.com>
2024-03-20 22:02:53 +0900

    [TS-276] Refactor: 별 배경 리팩토링
    - 기존 별 배경 및 애니메이션을 framer motion으로 변경(좀 더 알아보기 쉬워서 framer motion으로 변경함)
    - 필요없는 주석 삭제
    - 별 만드는 로직 함수로 분리(generateStars)
    - 별 개수 200개로 줄임

--- src/components/StarPage/StarMain/StarBackground.tsx
+++ src/components/StarPage/StarMain/StarBackground.tsx
@@ -1,23 +1,30 @@
 import { css } from "@emotion/react";
 
+import { motion } from "framer-motion";
+
 interface StarProps {
 	cx: number; // 중심 x 좌표
 	cy: number; // 중심 y 좌표
-	r: number; // 원의 반지름
+	r: number; // 원 반지름(별의 크기)
 }
 
-export default function StarBackground() {
+function generateStars() {
 	const stars = [];
-	const numberOfStars = 500; // 생성할 별의 수
+	const numberOfStars = 200;
 
 	for (let i = 0; i < numberOfStars; i++) {
 		const cx = Math.random() * window.innerWidth;
 		const cy = Math.random() * window.innerHeight;
-		const r = Math.random() * 1; // 별의 반지름을 랜덤하게 설정 (별의 크기)
+		const r = Math.random() * 1;
 		stars.push(<Star key={`star-${i}`} cx={cx} cy={cy} r={r} />);
 	}
 
-	// TODO: 100dvh 상수로 만들기...
+	return stars;
+}
+
+export default function StarBackground() {
+	const stars = generateStars();
+
 	return (
 		<svg width="500px" height="100dvh" css={starStyle}>
 			{stars}
@@ -26,23 +33,28 @@ export default function StarBackground() {
 }
 
 function Star({ cx, cy, r }: StarProps) {
-	const duration = Math.random() * 2 + 1; // 1초에서 3초 사이의 지속 시간
-	const screenHeight = window.innerHeight; // 화면 높이
+	const duration = Math.random() * 2 + 1; // 1초에서 3초 사이
+	const screenHeight = window.innerHeight;
 	const opacity = 1 - cy / screenHeight; // 화면 아래로 갈수록 투명도가 증가
 
+	const animation = {
+		scale: [1, 0.5, 1], // 크기가 줄었다가 다시 커지는 효과
+		transition: {
+			duration: duration,
+			repeat: Infinity,
+		},
+	};
+
 	return (
-		<circle cx={cx} cy={cy} r={r} fill="white" fill-opacity={opacity}>
-			<animate
-				attributeName="r"
-				from={r}
-				to={r * 0.5} // 최소 크기를 별의 반지름의 절반으로 설정
-				dur={`${duration}s`} // 지속 시간 설정
-				repeatCount="indefinite" // 무한 반복
-				begin={`${Math.random() * duration}s`} // 애니메이션 시작 지연
-				keyTimes="0; 0.5; 1" // 애니메이션의 중간 지점에서 가장 작게 설정
-				values={`${r}; ${r * 0.5}; ${r}`} // 크기가 줄었다가 다시 커지는 효과
-			/>
-		</circle>
+		<motion.circle
+			initial={{ cx, cy }} // cx, cy 초기값 고정
+			cx={cx}
+			cy={cy}
+			r={r}
+			fill="white"
+			animate={animation}
+			fillOpacity={opacity}
+		/>
 	);
 }
 
