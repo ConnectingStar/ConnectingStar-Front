5348eb4a96cdd0603608e0ea6dc4a283cc22f9e9
pizzaYami <cyj9407@naver.com>
2024-03-20 17:50:51 +0900

    [TS-25] Feat: ChattingPage 완성

---
+++ src/components/Chatting/ChattingMessage.tsx
@@ -0,0 +1,108 @@
+import { useEffect, useRef, useState } from "react";
+
+import FooterBtn from "@/components/common/FooterBtn/FooterBtn";
+
+import { theme } from "@/styles/theme";
+
+import { chattingStyle, replyStyle } from "@/components/Chatting/ChattingMessage.style";
+
+interface chatType {
+	chatData: { id: string; message: string[]; replyBtnMessage: string[]; reply: string };
+	setProgress: React.Dispatch<React.SetStateAction<number>>;
+}
+
+function ChattingMessage({ chatData, setProgress }: chatType) {
+	const { id, message, replyBtnMessage, reply } = chatData;
+
+	const [messageIndex, setMessageIndex] = useState(0);
+	const [isreply, setIsReply] = useState(false);
+	const endOfMessagesRef = useRef<HTMLDivElement>(null);
+
+	// 메시지 하나씩 내려오게하는 함수
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			if (message.length > messageIndex) {
+				setMessageIndex((prevIndex) => prevIndex + 1);
+			}
+		}, 100);
+
+		return () => clearTimeout(timer);
+	}, [messageIndex]);
+
+	// 스크롤다운 함수
+	const scrollToBottom = () => {
+		endOfMessagesRef.current?.scrollIntoView({ behavior: "smooth" });
+	};
+
+	useEffect(() => {
+		scrollToBottom();
+	}, [messageIndex]);
+
+	// 버튼 함수
+	const handleReplyBtn = () => {
+		setProgress((prev) => prev + 1);
+		setMessageIndex((prevIndex) => prevIndex + 1);
+		setIsReply(true);
+	};
+
+	const sideBtnStyle = () => {
+		if (replyBtnMessage.length === 3) return chattingStyle.sideButton;
+		if (replyBtnMessage.length > 3) return chattingStyle.scrollButton;
+		return null;
+	};
+
+	// 특정 메시지의 단어 파란색으로 변경하는 함수
+	function processMessage(message: string, id: string) {
+		if (id === "time") {
+			return message.replace(
+				/(명확|매력|쉽게|만족)/g,
+				(match) => `<span style="color: ${theme.color.main_blue};">${match}</span>`,
+			);
+		} else if (id === "last") {
+			return message.replace(
+				/(매일 실행|꾸준하게 하는 것)/g,
+				(match) => `<span style="color: ${theme.color.main_blue};">${match}</span>`,
+			);
+		}
+		return message;
+	}
+
+	return (
+		<div css={chattingStyle.container}>
+			<div css={chattingStyle.profile}>
+				<img src="" alt="profile" />
+			</div>
+			<div css={chattingStyle.chatWrap} ref={endOfMessagesRef}>
+				<ul>
+					{message.slice(0, messageIndex + 1).map((msg, index) => (
+						<li
+							key={index}
+							dangerouslySetInnerHTML={{ __html: processMessage(msg, chatData.id) }}
+						></li>
+					))}
+				</ul>
+
+				{isreply ? (
+					<div css={replyStyle} dangerouslySetInnerHTML={{ __html: reply }}></div>
+				) : (
+					(id === "alert" || id === "organize") && (
+						<div css={replyStyle} dangerouslySetInnerHTML={{ __html: reply }}></div>
+					)
+				)}
+			</div>
+
+			{message.length === messageIndex && (
+				<div>
+					<div css={sideBtnStyle}>
+						{replyBtnMessage.slice(1).map((item) => (
+							<button>{item}</button>
+						))}
+					</div>
+					<FooterBtn text={replyBtnMessage[0]} handleBtnClick={handleReplyBtn} isTransparent />
+				</div>
+			)}
+		</div>
+	);
+}
+
+export default ChattingMessage;
--- src/pages/ChattingPage.tsx
+++ src/pages/ChattingPage.tsx
@@ -1,3 +1,18 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+
+import { css } from "@emotion/react";
+
+import ChattingMessage from "@/components/Chatting/ChattingMessage";
+import Progressbar from "@/components/Chatting/Progressbar";
+import Header from "@/components/common/Header/Header";
+
+import { RootState } from "@/api/store";
+
+import { createChatData } from "@/constants/chatData";
+
+import { theme } from "@/styles/theme";
+
 // import LocationModal from "@/components/Chatting/LocationModal";
 // import SelectTagModal from "@/components/Chatting/SelectTagModal";
 
@@ -17,12 +32,52 @@
 // ];
 
 function ChattingPage() {
+	const [progress, setProgress] = useState(0);
+
+	const userData = useSelector((state: RootState) => state.userData);
+	const chatData = createChatData(userData);
+	const isbuttonHeiger = chatData[progress].replyBtnMessage.length > 1;
 	return (
 		<div>
-			{/* <LocationModal /> */}
-			{/* <SelectTagModal title="어떤 습관을 만들어 볼까요?" tags={habitTags} /> */}
+			<div css={chattingPageStyle.container}>
+				<div css={chattingPageStyle.header}>
+					<Header>
+						<Header.PrevButton></Header.PrevButton>
+					</Header>
+					<Progressbar currentClicks={progress} totalClicksNeeded={11} />
+				</div>
+				<div css={chattingPageStyle.chattingWrap(isbuttonHeiger)}>
+					{chatData.slice(0, progress + 1).map((chatData) => (
+						<ChattingMessage
+							key={chatData.id}
+							chatData={chatData}
+							setProgress={setProgress}
+						></ChattingMessage>
+					))}
+				</div>
+				{/* <LocationModal /> */}
+				{/* <SelectTagModal title="어떤 습관을 만들어 볼까요?" tags={habitTags} /> */}
+			</div>
 		</div>
 	);
 }
 
 export default ChattingPage;
+
+const chattingPageStyle = {
+	container: css`
+		max-width: 22.5rem;
+		margin: 0 auto;
+		min-height: 100vh;
+		background-color: ${theme.color.bg};
+	`,
+	header: css`
+		position: fixed;
+		top: 0;
+	`,
+	chattingWrap: (isbuttonHeiger: boolean) => css`
+		padding: 5.75rem 1.5rem ${isbuttonHeiger ? "8.5rem" : "5.438rem"} 1.5rem;
+		margin: 0 auto;
+		${theme.font.body_b};
+	`,
+};
