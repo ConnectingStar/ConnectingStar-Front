23c179bbfd8d85cbc41dd6bc144ca489d99280b4
pizzaYami <cyj9407@naver.com>
2024-03-20 18:06:20 +0900

    [TS-25] Design: style 컴포넌트하단에 위치

--- src/components/Chatting/Progressbar.tsx
+++ src/components/Chatting/Progressbar.tsx
@@ -7,18 +7,8 @@ interface ProgressProps {
 	currentClicks: number;
 }
 
-const progressbarStyle = (calculateProgress: number) => css`
-	width: 100%;
-	height: 4px;
-	background-color: ${theme.color.button_disabled};
-	& > div {
-		height: 4px;
-		width: ${calculateProgress}%;
-		background-color: ${theme.color.main_blue};
-	}
-`;
-
 function Progressbar({ totalClicksNeeded, currentClicks }: ProgressProps) {
+	// 클릭 필요 수와 현재 클릭 수를 비교해서 %로 보여주는 함수
 	const calculateProgress = () => {
 		return (currentClicks / totalClicksNeeded) * 100;
 	};
@@ -31,3 +21,14 @@ function Progressbar({ totalClicksNeeded, currentClicks }: ProgressProps) {
 }
 
 export default Progressbar;
+
+const progressbarStyle = (calculateProgress: number) => css`
+	width: 100%;
+	height: 4px;
+	background-color: ${theme.color.button_disabled};
+	& > div {
+		height: 4px;
+		width: ${calculateProgress}%;
+		background-color: ${theme.color.main_blue};
+	}
+`;
