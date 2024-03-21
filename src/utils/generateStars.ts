9a1a200c12e927dd7b78550b6f5ccbe4b4dde501
khkh0109 <ist200120@gmail.com>
2024-03-21 15:01:53 +0900

    [TS-276] Refactor: 별 반지름 설정하는 Math.random()에 * 1 삭제
    Math.random은 원래 1 미만인 값을 리턴하기 때문에 1을 곱하는 의미가 없어서 삭제함

--- src/utils/generateStars.ts
+++ src/utils/generateStars.ts
@@ -5,7 +5,7 @@ export default function generateStars() {
 	for (let i = 0; i < numberOfStars; i++) {
 		const cx = Math.random() * window.innerWidth; // 별의 위치 랜덤 설정
 		const cy = Math.random() * window.innerHeight; // 별의 위치 랜덤 설정
-		const r = Math.random() * 1; // 별의 반지름 랜덤 설정 (별의 크기)
+		const r = Math.random(); // 별의 반지름 랜덤 설정 (별의 크기)
 		stars.push({
 			id: i,
 			cx,
