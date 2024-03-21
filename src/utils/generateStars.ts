f1d317fed34d87f2f01d112a40f2cb071bf12c47
khkh0109 <ist200120@gmail.com>
2024-03-21 14:52:45 +0900

    [TS-276] Comment: generateStars 함수에 필요한 주석 추가

--- src/utils/generateStars.ts
+++ src/utils/generateStars.ts
@@ -3,9 +3,9 @@ export default function generateStars() {
 	const numberOfStars = 200;
 
 	for (let i = 0; i < numberOfStars; i++) {
-		const cx = Math.random() * window.innerWidth;
-		const cy = Math.random() * window.innerHeight;
-		const r = Math.random() * 1;
+		const cx = Math.random() * window.innerWidth; // 별의 위치 랜덤 설정
+		const cy = Math.random() * window.innerHeight; // 별의 위치 랜덤 설정
+		const r = Math.random() * 1; // 별의 반지름 랜덤 설정 (별의 크기)
 		stars.push({
 			id: i,
 			cx,
