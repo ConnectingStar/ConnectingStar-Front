bb560d2045e6755f8383fb318ce561a39310b993
khkh0109 <ist200120@gmail.com>
2024-03-21 14:49:02 +0900

    [TS-276] Refactor: 필요없는 CSS 속성 삭제, getOutlineTextStyle 함수에서 특정 요소에서만 사용하는 속성 삭제
    white-space, text-align은 StarButton 텍스트에서만 사용하는 속성. 다른 아웃라인 텍스트 스타일에는 필요없어서 삭제함

--- src/components/StarPage/StarMain/getOutlineText.style.ts
+++ src/components/StarPage/StarMain/getOutlineText.style.ts
@@ -9,14 +9,11 @@ export const getOutlineTextStyle = (outlineColor: string, font: FontType) => {
 		position: relative;
 		z-index: 0;
 		${theme.font[font]}
-		white-space: pre-wrap;
-		text-align: center;
 		color: #fff;
 
 		&:after {
 			content: attr(data-text);
 			display: block;
-			width: 100%;
 			position: absolute;
 			top: 0;
 			z-index: -1;
