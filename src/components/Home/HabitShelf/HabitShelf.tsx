06e48eb69cde16dd1db0268f3e353990164fcc44
김영택 <kooduck168@gmail.com>
2024-03-24 22:44:27 +0900

    [TS-190] Fix: habitShelf style 정리, path 수정

--- src/components/Home/HabitShelf/HabitShelf.tsx
+++ src/components/Home/HabitShelf/HabitShelf.tsx
@@ -6,39 +6,36 @@ function HabitShelf() {
 	const mode = ["휴식", "실천"];
 	const randomIndex = Math.floor(Math.random() * 10);
 	const today = new Date();
-
 	return (
-		<>
-			<main css={layoutStyle}>
-				<h1>{`${today.getMonth() + 1}월 ${today.getDate()}일\n영택님의 ${mode[randomIndex % 2]} 기록`}</h1>
-				{mode[randomIndex % 2] === "실천" && (
-					<>
-						<label>
-							<h2>{`정체성\n\n`}</h2>
-							<span>{`매일 성장하는 사람`}</span>
-						</label>
-						<section>
-							<h3>나는</h3>
-							<ul>
-								<li>오후 8시에</li>
-								<li>우리 집 안 내 책상 위에서</li>
-								<li>책 읽기를</li>
-								<li>5 페이지</li>
-							</ul>
-							<h3>했다</h3>
-						</section>
-						<div className="icon">
-							<h2>오늘의 습관 실천은</h2>
-							{habitIconData[randomIndex % 5].icon}
+		<main css={layoutStyle}>
+			<h1>{`${today.getMonth() + 1}월 ${today.getDate()}일\n영택님의 ${mode[randomIndex % 2]} 기록`}</h1>
+			{mode[randomIndex % 2] === "실천" && (
+				<>
+					<div>
+						<h2>정체성</h2>
+						{`매일 성장하는 사람`}
+					</div>
+					<div>
+						<h2>나는</h2>
+						<div>
+							<div>오후 8시에</div>
+							<div>우리 집 안 내 책상 위에서</div>
+							<div>책 읽기를</div>
+							<div>5페이지</div>
 						</div>
-					</>
-				)}
-				<div className="record">
-					<h2>별자취 남기기</h2>
-					<div>오늘은 목표량을 잘 달성하여 뿌듯하다! 앞으로도 열심히 해야겠다.</div>
-				</div>
-			</main>
-		</>
+						<h2>했다</h2>
+					</div>
+					<div>
+						<h2>오늘의 습관 실천은</h2>
+						{habitIconData[randomIndex % 5].icon}
+					</div>
+				</>
+			)}
+			<div>
+				<h2>별자취 남기기</h2>
+				<div>오늘은 목표량을 잘 달성하여 뿌듯하다! 앞으로도 열심히 해야겠다.</div>
+			</div>
+		</main>
 	);
 }
 
