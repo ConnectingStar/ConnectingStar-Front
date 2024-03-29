8901be36956c188fcf4755cbe6aab7fc42d7cff5
김영택 <kooduck168@gmail.com>
2024-03-29 11:46:27 +0900

    [TS-190] Fix: habitShelf condition 분리

--- src/components/Home/HabitShelf/HabitShelf.tsx
+++ src/components/Home/HabitShelf/HabitShelf.tsx
@@ -1,4 +1,4 @@
-import { habitIconData } from "@/constants/myPageConstants";
+import Condition from "@/components/Home/HabitShelf/Condition";
 
 import { layoutStyle } from "@/components/Home/HabitShelf/HabitShelf.style";
 
@@ -9,28 +9,7 @@ function HabitShelf() {
 	return (
 		<main css={layoutStyle}>
 			<h1>{`${today.getMonth() + 1}월 ${today.getDate()}일\n영택님의 ${mode[randomIndex % 2]} 기록`}</h1>
-			{mode[randomIndex % 2] === "실천" && (
-				<>
-					<div>
-						<h2>정체성</h2>
-						<p>매일 성장하는 사람</p>
-					</div>
-					<div>
-						<h2>나는</h2>
-						<div>
-							<p>오후 8시에</p>
-							<p>우리 집 안 내 책상 위에서</p>
-							<p>책 읽기를</p>
-							<p>5페이지</p>
-						</div>
-						<h2>했다</h2>
-					</div>
-					<div>
-						<h2>오늘의 습관 실천은</h2>
-						{habitIconData[randomIndex % 5].icon}
-					</div>
-				</>
-			)}
+			{mode[randomIndex % 2] === "실천" && <Condition />}
 			<div>
 				<h2>별자취 남기기</h2>
 				<p>오늘은 목표량을 잘 달성하여 뿌듯하다! 앞으로도 열심히 해야겠다.</p>
