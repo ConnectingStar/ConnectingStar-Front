dee171c4c8734db2ef898f1978cfba282a903355
pizzaYami <cyj9407@naver.com>
2024-04-01 15:11:14 +0900

    [TS-297] Feat: Splash 이미지 연결

---
+++ src/components/Onboarding/Splash.tsx
@@ -0,0 +1,34 @@
+import { css } from "@emotion/react";
+
+import Charater from "@/assets/image/img-charater.png";
+import Logo from "@/assets/image/img-logo.png";
+function Splash({ onNext }: { onNext: () => void }) {
+	return (
+		<div css={container} onClick={onNext}>
+			<div>
+				<img src={Charater} alt="charater" />
+				<img src={Logo} alt="logo" />
+			</div>
+		</div>
+	);
+}
+
+export default Splash;
+
+const container = css`
+	width: 100%;
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	background-color: #020274;
+	& > div {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		width: 150px;
+		height: 200px;
+	}
+`;
