8ae6e8aec7967a133df2793e21c6d677c6510e5d
pizzaYami <cyj9407@naver.com>
2024-04-01 17:37:26 +0900

    [TS-297] Feat: CreateAccount 연결

--- src/components/Onboarding/CreateAccount.tsx
+++ src/components/Onboarding/CreateAccount.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import { css } from "@emotion/react";
+
 import ArrowDownIcon from "@/assets/icon/ic-arrow-down.svg?react";
 
 import FooterBtn from "@/components/common/FooterBtn/FooterBtn";
@@ -10,12 +12,13 @@ import SelectGenderModal from "@/components/common/Modal/CommonModal/SelectGende
 
 import { useAppDispatch, useAppSelector } from "@/api/hooks";
 import { openModal } from "@/api/modal/modalSlice";
+import { updataBasicUserData } from "@/api/user/userSlice";
 
 import { modalType } from "@/constants/modalConstants";
 
-import { createAccountStyle } from "@/components/Onboarding/CreateAccount.style";
+import { theme } from "@/styles/theme";
 
-export default function CreateAccount() {
+export default function CreateAccount({ onNext }: { onNext: () => void }) {
 	const dispatch = useAppDispatch();
 
 	const { modal } = useAppSelector((state) => state.modal);
@@ -45,14 +48,22 @@ export default function CreateAccount() {
 		},
 	];
 
+	const confirmBasicUserData = async () => {
+		if (nickName !== "" && gender !== "" && age !== "") {
+			await dispatch(updataBasicUserData({ nickName, gender, age }));
+		}
+		onNext();
+	};
+
 	return (
 		<>
 			<Header>
 				<Header.PrevButton />
 			</Header>
-			<div css={createAccountStyle.container}>
+
+			<div css={container}>
 				<h1>내 정보 입력을 완료해 주세요</h1>
-				<ul css={createAccountStyle.wrap}>
+				<ul css={wrap}>
 					{accountInputData.map((item) => (
 						<li key={item.title}>
 							<h2>{item.title}</h2>
@@ -63,14 +74,19 @@ export default function CreateAccount() {
 								}}
 							>
 								{item.state === "" ? item.content : item.state}
-
 								{item.title !== "닉네임" && <ArrowDownIcon />}
 							</div>
 						</li>
 					))}
 				</ul>
 			</div>
-			<FooterBtn text="다음" isTransparent disabled={!nickName || !gender || !age} />
+
+			<FooterBtn
+				text="다음"
+				isTransparent
+				disabled={!nickName || !gender || !age}
+				handleBtnClick={confirmBasicUserData}
+			/>
 
 			{modal === modalType.CHANGE_NICKNAME && <ChangeNicknameModal changeNickname={setNickName} />}
 			{modal === modalType.SELECT_GENDER && <SelectGenderModal changeGender={setGender} />}
@@ -78,3 +94,36 @@ export default function CreateAccount() {
 		</>
 	);
 }
+
+const container = css`
+	width: 22.5rem;
+	margin: 0 auto;
+	padding: 1.25rem 1.5rem;
+
+	& > h1 {
+		${theme.font.head_a}
+		margin-bottom: 2.5rem;
+	}
+`;
+
+const wrap = css`
+	& > li {
+		margin-bottom: 1.25rem;
+		& > h2 {
+			${theme.font.head_c}
+			color: ${theme.color.font_gray};
+			margin-bottom: 0.75rem;
+		}
+		& > div {
+			display: flex;
+			justify-content: space-between;
+			height: 3.438rem;
+			background-color: ${theme.color.bg};
+			color: ${theme.color.button_deactivated};
+			align-items: center;
+			${theme.font.body_a};
+			border-radius: 15px;
+			padding: 1rem;
+		}
+	}
+`;
--- src/pages/OnBoardingPage.tsx
+++ src/pages/OnBoardingPage.tsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 
+import CreateAccount from "@/components/Onboarding/CreateAccount";
 import OauthSignUp from "@/components/Onboarding/OauthSignup/OauthSignUp";
 import SignUp from "@/components/Onboarding/SignUp";
 
 // import Splash from "@/components/Onboarding/Splash";
 
 function OnboardingPage() {
-	const [step, setStep] = useState<"Splash" | "SignUp" | "OauthSignUp">("Splash");
+	const [step, setStep] = useState<
+		"Splash" | "SignUp" | "OauthSignUp" | "CreateAccount" | "VisitTracker"
+	>("CreateAccount");
 	console.log(step);
 
 	return (
@@ -28,7 +31,14 @@ function OnboardingPage() {
 			{step === "OauthSignUp" && (
 				<OauthSignUp
 					onNext={() => {
-						setStep("SignUp");
+						setStep("CreateAccount");
+					}}
+				/>
+			)}
+			{step === "CreateAccount" && (
+				<CreateAccount
+					onNext={() => {
+						setStep("VisitTracker");
 					}}
 				/>
 			)}
