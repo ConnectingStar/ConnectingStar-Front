f495731f655a248b4206767b7711d885ef132502
pizzaYami <cyj9407@naver.com>
2024-04-01 16:48:23 +0900

    [TS-297] Design: OauthSignUp css정리

--- src/components/OnBoarding/OauthSignup/OauthSignUp.tsx
+++ src/components/OnBoarding/OauthSignup/OauthSignUp.tsx
@@ -11,67 +11,81 @@ import { theme } from "@/styles/theme";
 
 function OauthSignUp({ onNext }: { onNext: () => void }) {
 	// TODO: oauth로그인 성공 후 다음페이지로 넘어가기
+	// TODO: header fixed 넣기
 	return (
-		<div css={container}>
-			<Header>
-				<Header.CloseButton />
-			</Header>
-			<div css={wrap}>
-				<img src={LogoImg} alt="logo" />
-				<div css={oauth}>
-					<button type="button" onClick={onNext}>
-						<img src={kakao} alt="kakao" />
-						카카오로 계속하기
-					</button>
-					<button type="button" onClick={onNext}>
-						<img src={google} alt="google" />
-						구글로 계속하기
-					</button>
+		<>
+			<div css={header}>
+				<Header>
+					<Header.CloseButton />
+				</Header>
+			</div>
+			<div css={container}>
+				<div css={wrap}>
+					<img src={LogoImg} alt="logo" />
+					<div>
+						<button type="button" onClick={onNext}>
+							<img src={kakao} alt="kakao" />
+							카카오로 계속하기
+						</button>
+						<button type="button" onClick={onNext}>
+							<img src={google} alt="google" />
+							구글로 계속하기
+						</button>
+					</div>
 				</div>
 			</div>
 			<FooterPrivacyPolicyLink />
-		</div>
+		</>
 	);
 }
 
 export default OauthSignUp;
 
+const header = css`
+	position: fixed;
+	left: 50%;
+	transform: translateX(-50%);
+	top: 0;
+`;
+
 const container = css`
 	display: flex;
-	flex-direction: column;
-	justify-content: space-between;
+	justify-content: center;
 	width: 22.5rem;
 	height: 100dvh;
-	padding: 1.25rem 1.5rem 0;
+	padding: 3.438rem 1.5rem 0;
 	margin: 0 auto;
 `;
 
 const wrap = css`
 	display: flex;
 	flex-direction: column;
-	margin-bottom: 6.875rem;
 	align-items: center;
-	gap: 220px;
-`;
+	justify-content: flex-end;
+	width: 100%;
+	margin-bottom: 6.875rem;
+	gap: 12.5rem;
 
-const oauth = css`
-	width: 19.5rem;
-	display: flex;
-	flex-direction: column;
-	gap: 0.375rem;
-	& > button {
-		height: 3.438rem;
+	& > div {
+		width: 100%;
 		display: flex;
-		align-items: center;
-		justify-content: center;
-		border-radius: 15px;
-		gap: 0.313rem;
+		flex-direction: column;
+		gap: 0.375rem;
+		& > button {
+			width: 100%;
+			height: 3.438rem;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			border-radius: 15px;
+			gap: 0.313rem;
 
-		&:first-of-type {
-			background-color: ${theme.color.main_yellow};
-		}
-		&:last-of-type {
-			border: 2px solid ${theme.color.line};
+			&:first-of-type {
+				background-color: ${theme.color.main_yellow};
+			}
+			&:last-of-type {
+				border: 2px solid ${theme.color.line};
+			}
 		}
 	}
 `;
