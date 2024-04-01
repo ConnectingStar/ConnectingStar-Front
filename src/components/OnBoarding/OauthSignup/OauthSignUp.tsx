593bd5f293c697471a319afd6fcdf3b7b5334185
pizzaYami <cyj9407@naver.com>
2024-04-01 18:23:37 +0900

    [TS-297] Rename: kakao -> KakaoIcon

--- src/components/OnBoarding/OauthSignup/OauthSignUp.tsx
+++ src/components/OnBoarding/OauthSignup/OauthSignUp.tsx
@@ -1,7 +1,7 @@
 import { css } from "@emotion/react";
 
-import google from "@/assets/icon/ic-signup.google.svg";
-import kakao from "@/assets/icon/ic-signup.kakao.svg";
+import GoogleIcon from "@/assets/icon/ic-signup.google.svg";
+import KakaoIcon from "@/assets/icon/ic-signup.kakao.svg";
 import LogoImg from "@/assets/image/img-logo-black.png";
 
 import FooterPrivacyPolicyLink from "./FooterPrivacyPolicyLink";
@@ -24,11 +24,11 @@ function OauthSignUp({ onNext }: { onNext: () => void }) {
 					<img src={LogoImg} alt="logo" />
 					<div>
 						<button type="button" onClick={onNext}>
-							<img src={kakao} alt="kakao" />
+							<img src={KakaoIcon} alt="kakao-icon" />
 							카카오로 계속하기
 						</button>
 						<button type="button" onClick={onNext}>
-							<img src={google} alt="google" />
+							<img src={GoogleIcon} alt="google-icon" />
 							구글로 계속하기
 						</button>
 					</div>
@@ -67,19 +67,18 @@ const wrap = css`
 	gap: 12.5rem;
 
 	& > div {
-		width: 100%;
 		display: flex;
 		flex-direction: column;
+		width: 100%;
 		gap: 0.375rem;
 		& > button {
-			width: 100%;
-			height: 3.438rem;
 			display: flex;
 			align-items: center;
 			justify-content: center;
+			width: 100%;
+			height: 3.438rem;
 			border-radius: 15px;
 			gap: 0.313rem;
-
 			&:first-of-type {
 				background-color: ${theme.color.main_yellow};
 			}
