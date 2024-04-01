130f235a314f2abfadfc2c4a96d5f562a8556bf0
pizzaYami <cyj9407@naver.com>
2024-04-01 18:30:59 +0900

    [TS-297] Feat: 테스트용 useSelector 추가

--- src/pages/OnBoardingPage.tsx
+++ src/pages/OnBoardingPage.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 import CreateAccount from "@/components/Onboarding/CreateAccount";
@@ -12,7 +13,11 @@ function OnboardingPage() {
 	const [step, setStep] = useState<
 		"Splash" | "SignUp" | "OauthSignUp" | "CreateAccount" | "VisitorRoute"
 	>("Splash");
-	console.log(step);
+
+	//TODO: 테스트용 merge후 제거 예정
+	// VisitorRoute 빼고는 확인가능
+	const user = useSelector((state) => state.user);
+	console.log(user);
 
 	return (
 		<main>
