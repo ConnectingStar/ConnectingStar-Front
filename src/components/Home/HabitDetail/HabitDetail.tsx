65936019f066c352e05694dd81ec8c411ba90f80
김영택 <kooduck168@gmail.com>
2024-04-01 22:29:02 +0900

    [TS-190] Feat: habitShelf => habitDetail로 변경

--- src/components/Home/HabitDetail/HabitDetail.tsx
+++ src/components/Home/HabitDetail/HabitDetail.tsx
@@ -1,15 +1,15 @@
-import Condition from "@/components/Home/HabitShelf/Condition";
+import Action from "@/components/Home/HabitDetail/Action";
 
-import { layoutStyle } from "@/components/Home/HabitShelf/HabitShelf.style";
+import { layoutStyle } from "@/components/Home/HabitDetail/HabitDetail.style";
 
-function HabitShelf() {
+function HabitDetail() {
 	const mode = ["휴식", "실천"];
 	const randomIndex = Math.floor(Math.random() * 10);
 	const today = new Date();
 	return (
 		<main css={layoutStyle}>
 			<h1>{`${today.getMonth() + 1}월 ${today.getDate()}일\n영택님의 ${mode[randomIndex % 2]} 기록`}</h1>
-			{mode[randomIndex % 2] === "실천" && <Condition />}
+			{mode[randomIndex % 2] === "실천" && <Action />}
 			<div>
 				<h2>별자취 남기기</h2>
 				<p>오늘은 목표량을 잘 달성하여 뿌듯하다! 앞으로도 열심히 해야겠다.</p>
@@ -18,4 +18,4 @@ function HabitShelf() {
 	);
 }
 
-export default HabitShelf;
+export default HabitDetail;
