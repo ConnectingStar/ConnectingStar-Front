67b69e6c5e3864037b6db769fcfdb14705fee3ea
khkh0109 <ist200120@gmail.com>
2024-04-04 15:25:03 +0900

    [TS-245] Feat: 별자리 선택 모달 추가

---
+++ src/components/StarPage/Modal/SelectStarModal.tsx
@@ -0,0 +1,48 @@
+import { css } from "@emotion/react";
+
+import FooterBtn from "@/components/common/FooterBtn/FooterBtn";
+import Modal from "@/components/common/Modal/Modal";
+
+import { useAppDispatch } from "@/api/hooks";
+import { closeModal } from "@/api/modal/modalSlice";
+
+import { theme } from "@/styles/theme";
+
+export default function SelectStarModal() {
+	const dispatch = useAppDispatch();
+
+	return (
+		<Modal>
+			<div css={containerStyle}>
+				<h1>이 별자리로 선택할까요? </h1>
+				<p>
+					한 번 선택하면 완료할 때까지 바꿀 <br /> 수 없어요! 그대로 진행할까요?
+				</p>
+				<FooterBtn
+					text="선택"
+					leftText="취소"
+					handleLeftBtnClick={() => dispatch(closeModal())}
+					isPositionStatic
+				/>
+			</div>
+		</Modal>
+	);
+}
+
+const containerStyle = css`
+	width: 18rem;
+	padding: 1rem;
+	border-radius: 15px;
+	color: ${theme.color.font_black};
+	background-color: #fff;
+
+	h1 {
+		margin-bottom: 1.25rem;
+		font-size: 1.125rem;
+		font-weight: 700;
+	}
+
+	p {
+		margin-bottom: 2.5rem;
+	}
+`;
