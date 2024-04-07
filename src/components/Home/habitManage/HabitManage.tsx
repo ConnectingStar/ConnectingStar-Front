227585cdf9cd8cc7906c67f0a5c7ec5ce759835f
김영택 <kooduck168@gmail.com>
2024-04-07 23:51:01 +0900

    [TS-288] Fix: AlarmCheckModal 로직 변경

--- src/components/Home/habitManage/HabitManage.tsx
+++ src/components/Home/habitManage/HabitManage.tsx
@@ -1,12 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import AlarmCheckModal from "@/components/Home/habitManage/AlarmCheckModal/AlarmCheckModal";
-import HabitAlarmBox from "@/components/Home/habitManage/HabitAlarmBox";
+import Button from "@/components/Home/habitManage/Button/Button";
 
 import { useAppDispatch, useAppSelector } from "@/api/hooks";
 import { openModal } from "@/api/modal/modalSlice";
 
 import { habitManageConditions, habitManageAlarms } from "@/constants/homeConstants";
+import type { AlarmData } from "@/constants/homeConstants";
 import { modalType } from "@/constants/modalConstants";
 
 import {
@@ -15,29 +16,32 @@ import {
 	quitButtonStyle,
 } from "@/components/Home/habitManage/HabitManage.style";
 
-interface Alarm {
-	first: boolean;
-	second: boolean;
-	[key: string]: boolean;
-}
-
 function HabitManage() {
 	const dispatch = useAppDispatch();
 	const { modal } = useAppSelector((state) => state.modal);
-	const [target, setTarget] = useState<string>("");
-	const [alarm, setAlarm] = useState<Alarm>({
-		first: false,
-		second: false,
-	});
-
-	const CheckAlarm = (target: string) => {
-		setTarget(target);
-		if (!alarm[target]) {
-			dispatch(openModal(modalType.ALARM_CHECK));
-		} else {
-			setAlarm({ ...alarm, [target]: false });
-		}
+	const [alarmsData, setAlarmsData] = useState<AlarmData[]>(habitManageAlarms);
+	const [targetKey, setTargetKey] = useState<number>(0);
+	const alarmQuestion = (key: number) => {
+		setTargetKey(key);
+		dispatch(openModal(modalType.ALARM_CHECK));
 	};
+
+	const alarmCheck = (key: number) => {
+		const targetIdx = alarmsData.findIndex((alarm) => {
+			return alarm.key === key;
+		});
+		const targetData = alarmsData[targetIdx];
+		targetData.isActive = !targetData.isActive;
+		setAlarmsData([
+			...alarmsData.slice(0, targetIdx),
+			targetData,
+			...alarmsData.slice(targetIdx + 1),
+		]);
+	};
+
+	useEffect(() => {
+		console.log(alarmsData);
+	}, [alarmsData]);
 
 	return (
 		<main css={layoutStyle}>
@@ -48,7 +52,7 @@ function HabitManage() {
 			<div>
 				<span>습관</span>
 				{habitManageConditions.map((condition) => (
-					<div css={conditionStyle}>
+					<div key={condition.title} css={conditionStyle}>
 						<p>{condition.title}</p>
 						<p>{condition.input}</p>
 					</div>
@@ -56,13 +60,13 @@ function HabitManage() {
 			</div>
 			<div>
 				<span>알림</span>
-				{habitManageAlarms.map((inputs) => (
-					<HabitAlarmBox inputs={inputs} alarm={alarm} CheckAlarm={CheckAlarm} />
+				{alarmsData.map((inputs) => (
+					<Button inputs={inputs} isToggle alarmCheck={alarmCheck} alarmQuestion={alarmQuestion} />
 				))}
 			</div>
 			<button css={quitButtonStyle}>습관 그만두기</button>
 			{modal === modalType.ALARM_CHECK && (
-				<AlarmCheckModal target={target} alarm={alarm} setAlarm={setAlarm} />
+				<AlarmCheckModal targetKey={targetKey} alarmCheck={alarmCheck} />
 			)}
 		</main>
 	);
