cc02e5dc3b1b0ef40309bea8235b2e3c1d54135f
김영택 <kooduck168@gmail.com>
2024-04-11 18:53:27 +0900

    [TS-308] Fix: advices 롤링 매커니즘 변경, 탭 추가

--- src/components/Home/Landing/Advices/Advices.tsx
+++ src/components/Home/Landing/Advices/Advices.tsx
@@ -2,10 +2,17 @@ import { useState, useRef, useEffect } from "react";
 
 import { adviceImages } from "@/constants/homeConstants";
 
-import { containerStyle, advicesStyle } from "@/components/Home/Landing/Advices/Advices.style";
+import {
+	advicesWrapperStyle,
+	containerStyle,
+	advicesStyle,
+	listStyle,
+} from "@/components/Home/Landing/Advices/Advices.style";
 
 function Advices() {
 	const containerRef = useRef<HTMLDivElement | null>(null);
+	const timeoutRef = useRef(0);
+	const timeintervalRef = useRef(0);
 
 	const [currentIndex, setCurrentIndex] = useState<number>(0);
 
@@ -13,30 +20,48 @@ function Advices() {
 		if (!containerRef.current) {
 			return;
 		}
-
-		const intervalImages = setInterval(() => {
-			setCurrentIndex((currentIndex + 1) % 5);
-		}, 4000);
+		const scrollWidth = containerRef.current.scrollWidth - containerRef.current.offsetWidth;
 
 		containerRef.current.scrollTo({
-			left:
-				(containerRef.current.scrollWidth - containerRef.current.offsetWidth) * (currentIndex / 4),
+			left: (scrollWidth / 4) * (currentIndex % 5),
 			behavior: "smooth",
 		});
-
-		return () => clearInterval(intervalImages);
 	}, [currentIndex]);
 
+	const handleScroll = () => {
+		if (!containerRef.current) return;
+
+		clearTimeout(timeoutRef.current);
+		clearInterval(timeintervalRef.current);
+		const pageWidth = (containerRef.current.scrollWidth - containerRef.current.offsetWidth) / 4;
+		const index = Math.floor((containerRef.current.scrollLeft + pageWidth / 2) / pageWidth);
+
+		timeoutRef.current = setTimeout(() => {
+			setCurrentIndex(index);
+		}, 250);
+
+		timeintervalRef.current = setInterval(() => {
+			setCurrentIndex((prev) => prev + 1);
+		}, 4000);
+	};
+
 	return (
-		<section css={containerStyle} ref={containerRef}>
-			<ul css={advicesStyle}>
-				{adviceImages.map((image) => (
-					<li key={image.src}>
-						<img src={image.src} alt={image.alt} />
-					</li>
-				))}
+		<div css={advicesWrapperStyle}>
+			<div css={containerStyle} ref={containerRef} onScroll={handleScroll}>
+				<ul css={advicesStyle}>
+					{adviceImages.map((image) => (
+						<li key={image.src}>
+							<img src={image.src} alt={image.alt} />
+						</li>
+					))}
+				</ul>
+			</div>
+			<ul>
+				{Array.from({ length: 5 }).map((_, idx) => {
+					return <li key={idx} css={listStyle(idx === currentIndex)} />;
+				})}
 			</ul>
-		</section>
+		</div>
 	);
 }
 
