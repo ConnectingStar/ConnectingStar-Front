8027319cfc3007f87c1a3b3d901ef84979c2d333
sikkzz <sling0623@gmail.com>
2024-04-30 00:36:02 +0900

    [TS-325] Refactor: kakao login page

--- src/pages/KakaoLoginPage.tsx
+++ src/pages/KakaoLoginPage.tsx
@@ -1,12 +1,44 @@
 import { useEffect } from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 
-import { axiosInstance } from "@/api/axiosInstance";
+import axios from "axios";
+
+import { END_POINTS } from "@/constants/api";
+
+// 해당 페이지에서 이미 회원가입이 된 유저면 서버단에서는 현재 보이는 에러 페이지가 아니라 리다이렉트를 home으로 시켜줘야합니다.
 
 function KakaoLoginPage() {
 	const navigate = useNavigate();
+
 	const [searchParams] = useSearchParams();
+
 	const authCode = searchParams.get("code");
+
+	const fetchAccessToken = async (authCode: string) => {
+		try {
+			// api 주소 삭제 및 constants 처리
+			const response = await axios.post(
+				END_POINTS.LOGIN(authCode),
+				{},
+				{ withCredentials: true, headers: { "Content-Type": "application/json" } },
+			);
+
+			if (response.status === 200) {
+				navigate("/onboarding?step=CreateAccount");
+			}
+
+			// 불필요 코드 url이 서버단이기 때문에 어차피 작동을 안합니다 확인하시면 지워주세요
+			// if (response.status === 403) {
+			// 	navigate("/");
+			// }
+		} catch (error) {
+			// 에러날 가능성이 없지만 에러시 소셜 로그인 버튼 페이지로 이동
+			navigate("/onboarding?step=OauthSignUp");
+
+			console.error("error fetching access token:", error);
+		}
+	};
 
 	useEffect(() => {
 		if (authCode !== null) {
@@ -14,26 +46,7 @@ function KakaoLoginPage() {
 		}
 	}, [authCode]);
 
-	const fetchAccessToken = async (authCode: string) => {
-		const tokenUrl = `/oauth/login?socialType=K&authCode=${authCode}`;
-		//TODO: firstVisit대신 나중에 백엔드에서 DB 보냄
-		const firstVisit = localStorage.getItem("First visit");
-		try {
-			const response = await axiosInstance.post(tokenUrl);
-			if (response.status === 200) {
-				firstVisit === "false" && navigate("/");
-				navigate("/onboarding?step=CreateAccount");
-			}
-			// TODO: 나중에 수정 예정
-			if (response.status === 403) {
-				navigate("/");
-			}
-		} catch (error) {
-			console.error("error fetching access token:", error);
-		}
-	};
-
-	return <div></div>;
+	return <div />;
 }
 
 export default KakaoLoginPage;
