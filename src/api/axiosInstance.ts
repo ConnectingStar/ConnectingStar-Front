3da14a6358912062d0dec466028969155ce98345
pizzaYami <cyj9407@naver.com>
2024-04-29 19:09:28 +0900

    [TS-325] Feat: axiosInstance

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,11 +2,40 @@ import axios from "axios";
 
 import { BASE_URL } from "@/constants/api";
 
+import { getCookie } from "@/utils/getCookie";
+
 export const axiosInstance = axios.create({
 	baseURL: BASE_URL,
 	timeout: 3000,
-	// 원래 true임 fcm 테스트시 미인증 상태라 false로 해놓음
-	withCredentials: false,
+	withCredentials: true,
 });
 
-// 토큰 체크, 에러 코드 interceptors 추가 예정
+axiosInstance.interceptors.request.use(
+	(config) => {
+		const accessToken = getCookie("tars_token");
+		if (!accessToken) {
+			window.location.href = "/onboarding?step=CreateAccount";
+			return config;
+		}
+
+		config.headers["Content-Type"] = "application/json";
+
+		return config;
+	},
+	(error) => {
+		console.log(error);
+		return Promise.reject(error);
+	},
+);
+
+axiosInstance.interceptors.response.use(
+	(response) => {
+		return response;
+	},
+	(error) => {
+		if (error.response && error.response.status === 401) {
+			window.location.href = "/onboarding?step=CreateAccount";
+		}
+		return Promise.reject(error);
+	},
+);
