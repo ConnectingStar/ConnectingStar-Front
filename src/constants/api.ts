97ef40cf932ea880e48737587583d4476f08e5a0
pizzaYami <cyj9407@naver.com>
2024-05-03 17:31:22 +0900

    [TS-325] Fix: logout 엔드포인트 변경

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -3,5 +3,5 @@ export const BASE_URL = import.meta.env.VITE_BASE_URL;
 export const END_POINTS = {
 	FCM: "/api/fcm/register",
 	LOGIN: (authCode: string | null) => `${BASE_URL}/oauth/login?socialType=K&authCode=${authCode}`,
-	LOGOUT: "/oauth/unlink/kakao",
+	LOGOUT: "/oauth/logout",
 };
