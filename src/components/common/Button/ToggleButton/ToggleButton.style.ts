da7583aa1b182703b314fb66372b53379ded8031
sikkzz <sling0623@gmail.com>
2024-05-13 20:32:00 +0900

    [TS-330] Refactor: toggle button text max width add

--- src/components/common/Button/ToggleButton/ToggleButton.style.ts
+++ src/components/common/Button/ToggleButton/ToggleButton.style.ts
@@ -2,31 +2,30 @@ import { css } from "@emotion/react";
 
 import { theme } from "@/styles/theme";
 
-export const getLayoutStyle = (isDateText: boolean | undefined) => {
-	return css`
-		width: 19.5rem;
+export const getLayoutStyle = (isDateText: boolean | undefined, hasToggle?: boolean) => css`
+	width: 19.5rem;
+	border-radius: 15px;
+	background-color: ${theme.color.bg};
+	padding: 1rem;
+	margin-top: 0.375rem;
+
+	& > h4 {
+		color: ${isDateText ? theme.color.main_blue : theme.color.button_deactivated};
+		${!isDateText ? theme.font.body_c : theme.font.body_a};
+		margin-top: 0.125rem;
+		max-width: ${hasToggle ? "13.625rem" : ""};
+	}
+
+	& > button {
+		width: 17.5rem;
+		height: 3.4375rem;
+		margin-top: 1rem;
 		border-radius: 15px;
-		background-color: ${theme.color.bg};
-		padding: 1rem;
-		margin-top: 0.375rem;
-
-		& > h4 {
-			color: ${isDateText ? theme.color.main_blue : theme.color.button_deactivated};
-			${!isDateText ? theme.font.body_c : theme.font.body_a};
-			margin-top: 0.125rem;
-		}
-
-		& > button {
-			width: 17.5rem;
-			height: 3.4375rem;
-			margin-top: 1rem;
-			border-radius: 15px;
-			background-color: ${theme.color.main_blue};
-			${theme.font.head_b};
-			color: #fff;
-		}
-	`;
-};
+		background-color: ${theme.color.main_blue};
+		${theme.font.head_b};
+		color: #fff;
+	}
+`;
 
 export const getFlexStyle = css`
 	display: flex;
