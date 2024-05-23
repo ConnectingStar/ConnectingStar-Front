13a76a63080febd48de942d7f2fd244dd465f18a
sikkzz <sling0623@gmail.com>
2024-05-24 04:59:30 +0900

    [TS-335] Refactor: user profile refactoring

--- src/components/Habit/Landing/Profile/Profile.tsx
+++ src/components/Habit/Landing/Profile/Profile.tsx
@@ -1,27 +1,40 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 import ProfileButtonIcon from "@/assets/icon/ic-homepage-to-mypage.svg?react";
 import StarImage from "@/assets/image/img-card-detail-star-button.png";
 
+import { useAppDispatch, useAppSelector } from "@/api/hooks";
+import { getUserInfo } from "@/api/user/userThunk";
+
 import {
 	layoutStyle,
 	profileBoxStyle,
-	profileTextBoxStyle,
+	identityTextStyle,
+	nicknameTextStyle,
 } from "@/components/Habit/Landing/Profile/Profile.style";
 
 function Profile() {
+	const dispatch = useAppDispatch();
+
+	const { userData } = useAppSelector((state) => state.user);
+
 	const navigate = useNavigate();
+
+	useEffect(() => {
+		dispatch(getUserInfo());
+	}, []);
 
 	return (
 		<div css={layoutStyle}>
 			<div css={profileBoxStyle} onClick={() => navigate("/mypage")}>
 				<img src={StarImage} alt="user 프로필 이미지" />
-				<div css={profileTextBoxStyle}>
-					<span>매일 성장하는</span>
-					<span>사용자닉네임</span>
+				<div>
+					<p css={identityTextStyle}>{userData.identity}</p>
+					<p css={nicknameTextStyle}>{userData.nickname}</p>
 				</div>
 			</div>
-			<ProfileButtonIcon onClick={() => navigate("/habitgenerate")} />
+			<ProfileButtonIcon onClick={() => navigate("/habit-generate")} />
 		</div>
 	);
 }
