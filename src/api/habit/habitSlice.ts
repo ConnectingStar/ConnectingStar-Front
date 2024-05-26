9ffee34a341057cd66301fab037c2688aaa5bf5e
sikkzz <sling0623@gmail.com>
2024-05-26 17:47:08 +0900

    [TS-335] Feat: get habit history list api add

--- src/api/habit/habitSlice.ts
+++ src/api/habit/habitSlice.ts
@@ -5,16 +5,6 @@ import { getHabitHistoryList, getProgressHabitList } from "@/api/habit//habitThu
 const initialState = {
 	progressHabitList: [],
 	habitHistoryList: [],
-	// newHabitData: {
-	// 	identity: "",
-	// 	runTime: { noon: "오전", hour: "10", minute: "00" },
-	// 	place: "",
-	// 	behavior: "",
-	// 	behaviorValue: 0,
-	// 	behaviorUnit: "",
-	// 	firstAlert: { noon: "오전", hour: "09", minute: "50" },
-	// 	secondAlert: { noon: "오전", hour: "10", minute: "10" },
-	// },
 };
 
 const habitSlice = createSlice({
