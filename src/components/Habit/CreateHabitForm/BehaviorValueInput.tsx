0c8b8fc2ce49665e16de69f3337744c839c73a63
sikkzz <sling0623@gmail.com>
2024-05-29 03:48:24 +0900

    [TS-337] Refactor: form isActive style add

--- src/components/Habit/CreateHabitForm/BehaviorValueInput.tsx
+++ src/components/Habit/CreateHabitForm/BehaviorValueInput.tsx
@@ -13,10 +13,10 @@ const BehaviorValueInput = ({
 }: BehaviorValueInputProps) => {
 	return (
 		<div css={inputUnitBoxStyle}>
-			<div css={inputStyle("6.25rem")} onClick={handleModalOpen}>
+			<div css={inputStyle(inputValueData !== "", "6.25rem")} onClick={handleModalOpen}>
 				<span>{inputValueData === "" ? "숫자 입력" : inputValueData}</span>
 			</div>
-			<div css={inputStyle("12.875rem")} onClick={handleModalOpen}>
+			<div css={inputStyle(inputUnitData !== "", "12.875rem")} onClick={handleModalOpen}>
 				<span>{inputUnitData === "" ? "단위 입력 (예: 페이지)" : inputUnitData}</span>
 			</div>
 		</div>
