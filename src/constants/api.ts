69be235d1cd1ed5376876d229b1cb259810d85e6
khkh0109 <ist200120@gmail.com>
2024-05-28 15:38:33 +0900

    [TS-333] Feat: 별자리 카드 상세 엔드포인트 추가

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -19,6 +19,7 @@ export const END_POINTS = {
 	HABIT: "/habit",
 	HABIT_HISTORY_LIST: (runHabitId: number, increase: boolean, isRest: boolean) =>
 		`/habit/history?runHabitId=${runHabitId}&increase=${increase}&isRest=${isRest}`,
+	STAR_CARD_DETAIL: (id: string) => `constellation?constellationId=${id}`,
 };
 
 export const OAUTH_KAKAO_URL = `${BASE_URL}/oauth/code/url?socialType=K`;
