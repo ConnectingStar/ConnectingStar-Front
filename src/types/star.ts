5fe9d2894ea74247648a07761b14e0798133b9ec
khkh0109 <ist200120@gmail.com>
2024-05-28 15:44:27 +0900

    [TS-333] Feat: 별자리 카드 상세 타입 추가

---
+++ src/types/star.ts
@@ -0,0 +1,18 @@
+type TypeName = "육체활동" | "마음강화" | "지식습득" | "기록 습관" | "휴식 | 취미" | "";
+type StarCardDetailStatus = "SELECT" | "OTHER" | "PROGRESS" | "COMPLETE";
+
+interface StarCardDetailType {
+	constellationId: number;
+	typeName: TypeName;
+	name: string;
+	story: string;
+	identity: string;
+	image: string;
+	starCount: number;
+	status: StarCardDetailStatus;
+}
+
+export interface StarDataType {
+	isLoading: boolean;
+	starCardDetail: StarCardDetailType;
+}
