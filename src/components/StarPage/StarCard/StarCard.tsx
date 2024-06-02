2e3bc07eee204e0cd00385fb7e536ab9e0932d56
khkh0109 <ist200120@gmail.com>
2024-06-02 16:53:21 +0900

    [TS-343] Feat: 별자리 카드에 페이지 링크 추가

--- src/components/StarPage/StarCard/StarCard.tsx
+++ src/components/StarPage/StarCard/StarCard.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import starImg from "@/assets/image/img-card-detail-star.png";
 
 import { StarCardStatus } from "@/types/star";
@@ -12,6 +14,7 @@ import {
 } from "@/components/StarPage/StarCard/StarCard.style";
 
 interface StarCardProps {
+	id: number;
 	title: string;
 	subTitle: string;
 	starNumber: number;
@@ -19,22 +22,24 @@ interface StarCardProps {
 	state: StarCardStatus;
 }
 
-export default function StarCard({ title, subTitle, starNumber, image, state }: StarCardProps) {
+export default function StarCard({ id, title, subTitle, starNumber, image, state }: StarCardProps) {
 	return (
 		<li css={containerStyle}>
-			<div css={imgStyle}>
-				<img src={image} alt="별자리" />
-				<div css={starImgStyle}>
-					<img src={starImg} alt="노란색 별" />
-					<strong>{starNumber}</strong>
+			<Link to={`/star-card/${id}`}>
+				<div css={imgStyle}>
+					<img src={image} alt="별자리" />
+					<div css={starImgStyle}>
+						<img src={starImg} alt="노란색 별" />
+						<strong>{starNumber}</strong>
+					</div>
+					{state === "COMPLETE" && <div css={haveLabelStyle}>보유</div>}
 				</div>
-				{state === "COMPLETE" && <div css={haveLabelStyle}>보유</div>}
-			</div>
-			<div css={titleStyle}>
-				<strong>{subTitle}</strong>
-				<h3>{title}</h3>
-			</div>
-			{state === "PROGRESS" && <div css={selectedStyle}></div>}
+				<div css={titleStyle}>
+					<strong>{subTitle}</strong>
+					<h3>{title}</h3>
+				</div>
+				{state === "PROGRESS" && <div css={selectedStyle}></div>}
+			</Link>
 		</li>
 	);
 }
