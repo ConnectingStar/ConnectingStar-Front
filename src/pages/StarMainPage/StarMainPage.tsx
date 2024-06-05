107ddab36b5b62bed6c58b1a483c5de23835d50e
khkh0109 <ist200120@gmail.com>
2024-06-05 16:36:08 +0900

    [TS-346] Feat: 해금 시 모달 띄우는 기능 추가
    - 해금 시 모달 등장
    - 다른 페이지로 이동 후 다시 별자리 메인 페이지로 진입하면 모달 닫음

--- src/pages/StarMainPage/StarMainPage.tsx
+++ src/pages/StarMainPage/StarMainPage.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 
 import Gnb from "@/components/common/Gnb/Gnb";
-// import CharacterUnlockModal from "@/components/StarPage/Modal/CharacterUnlockModal";
+import CharacterUnlockModal from "@/components/StarPage/Modal/CharacterUnlockModal";
 import SelectStarButton from "@/components/StarPage/StarMain/SelectStarButton";
 import StarBackground from "@/components/StarPage/StarMain/StarBackground";
 import StarButton from "@/components/StarPage/StarMain/StarButton";
@@ -9,27 +9,31 @@ import StarCardLink from "@/components/StarPage/StarMain/StarCardLink";
 import StarCharacter from "@/components/StarPage/StarMain/StarCharacter";
 import StarInfo from "@/components/StarPage/StarMain/StarInfo";
 
-import { axiosInstance } from "@/api/axiosInstance";
 import { useAppDispatch, useAppSelector } from "@/api/hooks";
+import { openModal, closeModal } from "@/api/modal/modalSlice";
 import { getStarMain, addStarToConstellation } from "@/api/star/starThunk";
-// import { openModal } from "@/api/modal/modalSlice";
 
-// import { modalType } from "@/constants/modalConstants";
+import { modalType } from "@/constants/modalConstants";
 
 import { containerStyle, starMainPageGnbStyle } from "@/pages/StarMainPage/StarMainPage.style";
 
 export default function StarMainPage() {
 	const dispatch = useAppDispatch();
 	const { starMain } = useAppSelector((state) => state.star);
-	// const { modal } = useAppSelector((state) => state.modal);
+	const { isRegistered } = useAppSelector((state) => state.star);
+	const { modal } = useAppSelector((state) => state.modal);
 
-	// TODO: 테스트용 코드 (추후 별자리 메인 브랜치에서 수정 예정)
 	useEffect(() => {
-		// 현재 남은 별 개수 확인하는 API - 콘솔에서 확인 가능
-		axiosInstance.get("/user/star").then((data) => console.log(data.data.data.star));
-		// 현재 별자리 확인 API
 		dispatch(getStarMain());
 	}, []);
+
+	useEffect(() => {
+		if (isRegistered) {
+			dispatch(openModal(modalType.CHARACTER_UNLOCK));
+		} else {
+			dispatch(closeModal());
+		}
+	}, [isRegistered]);
 
 	const handleButtonClick = () => {
 		// TODO: 보유한 별 없으면 에러 발생 -> 버튼 disabled만 해두기..?
@@ -55,7 +59,9 @@ export default function StarMainPage() {
 				<Gnb />
 			</div>
 
-			{/* {modal === modalType.CHARACTER_UNLOCK && <CharacterUnlockModal />} */}
+			{modal === modalType.CHARACTER_UNLOCK && (
+				<CharacterUnlockModal id={starMain.constellationId} name={starMain.name} />
+			)}
 		</div>
 	);
 }
