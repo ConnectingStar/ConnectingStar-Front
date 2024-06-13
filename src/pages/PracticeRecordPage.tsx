a90ebfb4f89023e40c2a302f9fdc2df647af6d43
sikkzz <sling0623@gmail.com>
2024-06-14 07:01:07 +0900

    [TS-353] Refactor: habit record style refactoring

--- src/pages/PracticeRecordPage.tsx
+++ src/pages/PracticeRecordPage.tsx
@@ -1,12 +1,22 @@
+import { useNavigate, useParams } from "react-router-dom";
+
 import Header from "@/components/common/Header/Header";
 import HabitRecord from "@/components/Habit/HabitRecord/HabitRecord";
 
+import { PATH } from "@/constants/path";
+
 const PracticeRecordPage = () => {
+	const navigate = useNavigate();
+
+	const params = useParams();
+
 	return (
 		<>
 			<Header>
 				<Header.PrevButton />
-				<Header.TextButton>관리</Header.TextButton>
+				<Header.TextButton onClick={() => navigate(PATH.EDIT_HABIT(params.habitId))}>
+					관리
+				</Header.TextButton>
 			</Header>
 			<HabitRecord />
 		</>
