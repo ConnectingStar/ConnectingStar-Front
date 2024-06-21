521657e47d775395059d1d4b52fb1e049a555079
pizzaYami <cyj9407@naver.com>
2024-06-22 02:44:24 +0900

    [TS-356] Refactor: ChattingPage css구조변경

--- src/pages/ChattingPage.tsx
+++ src/pages/ChattingPage.tsx
@@ -38,96 +38,90 @@ function ChattingPage() {
 	}, []);
 
 	return (
-		<div>
+		<>
 			<Header>
 				<Header.PrevButton />
 			</Header>
-			<progress css={chattingPageStyle.progress} value={progress + 1} max={11} />
-			<div css={chattingPageStyle.container}>
-				<div css={chattingPageStyle.chattingWrap(isExtraBtn)}>
-					{chatData.slice(0, progress + 1).map((chatData) => (
-						<ChattingMessage
-							key={chatData.id}
-							chatData={chatData}
-							addProgress={() => setProgress((prev) => prev + 1)}
-						/>
-					))}
-				</div>
 
-				{modal === modalType.SELECT_BEHAVIOR && (
-					<SelectTagModal
-						title="어떤 습관을 만들어 볼까요?"
-						tags={SELECT_TAG_DATA.habitTags}
-						progress={progress}
-						addprogress={() => setProgress((prev) => prev + 1)}
+			<progress css={progressStyle} value={progress + 1} max={11} />
+
+			<div css={container(isExtraBtn)}>
+				{chatData.slice(0, progress + 1).map((chatData) => (
+					<ChattingMessage
+						key={chatData.id}
+						chatData={chatData}
+						addProgress={() => setProgress((prev) => prev + 1)}
 					/>
-				)}
-				{modal === modalType.SELECT_IDENTITY && (
-					<SelectTagModal
-						title="어떤 사람이 되고 싶으세요?"
-						tags={SELECT_TAG_DATA.identityTags}
-						progress={progress}
-						addprogress={() => setProgress((prev) => prev + 1)}
-					/>
-				)}
-				{modal === modalType.SELECT_TIME("RUNTIME") && (
-					<SelectTimeModal
-						title="시간을 선택해 주세요"
-						progress={progress}
-						addprogress={() => setProgress((prev) => prev + 1)}
-					/>
-				)}
-				{modal === modalType.SELECT_PLACE && (
-					<LocationModal progress={progress} addprogress={() => setProgress((prev) => prev + 1)} />
-				)}
-				{modal === modalType.SELECT_BEHAVIORUNIT && (
-					<BehaviorModal
-						behavior={userData.behavior}
-						progress={progress}
-						addprogress={() => setProgress((prev) => prev + 1)}
-					/>
-				)}
-				{modal === modalType.SELECT_TIME("FIRSTALERT") && (
-					<SelectTimeModal title="1차 알림시간을 선택해 주세요" />
-				)}
-				{modal === modalType.SELECT_TIME("SECONDALERT") && (
-					<SelectTimeModal title="2차 알림시간을 선택해 주세요" />
-				)}
+				))}
 			</div>
-		</div>
+
+			{modal === modalType.SELECT_BEHAVIOR && (
+				<SelectTagModal
+					title="어떤 습관을 만들어 볼까요?"
+					tags={SELECT_TAG_DATA.habitTags}
+					progress={progress}
+					addprogress={() => setProgress((prev) => prev + 1)}
+				/>
+			)}
+			{modal === modalType.SELECT_IDENTITY && (
+				<SelectTagModal
+					title="어떤 사람이 되고 싶으세요?"
+					tags={SELECT_TAG_DATA.identityTags}
+					progress={progress}
+					addprogress={() => setProgress((prev) => prev + 1)}
+				/>
+			)}
+			{modal === modalType.SELECT_TIME("RUNTIME") && (
+				<SelectTimeModal
+					title="시간을 선택해 주세요"
+					progress={progress}
+					addprogress={() => setProgress((prev) => prev + 1)}
+				/>
+			)}
+			{modal === modalType.SELECT_PLACE && (
+				<LocationModal progress={progress} addprogress={() => setProgress((prev) => prev + 1)} />
+			)}
+			{modal === modalType.SELECT_BEHAVIORUNIT && (
+				<BehaviorModal
+					behavior={userData.behavior}
+					progress={progress}
+					addprogress={() => setProgress((prev) => prev + 1)}
+				/>
+			)}
+			{modal === modalType.SELECT_TIME("FIRSTALERT") && (
+				<SelectTimeModal title="1차 알림시간을 선택해 주세요" />
+			)}
+			{modal === modalType.SELECT_TIME("SECONDALERT") && (
+				<SelectTimeModal title="2차 알림시간을 선택해 주세요" />
+			)}
+		</>
 	);
 }
 
 export default ChattingPage;
 
-const chattingPageStyle = {
-	container: css`
-		max-width: 22.5rem;
-		margin: 0 auto;
-		min-height: 100vh;
-		background-color: ${theme.color.bg};
-	`,
+const container = (isExtraBtn: boolean) => css`
+	max-width: 22.5rem;
+	min-height: 100vh;
+	padding: 5.75rem 1.5rem ${isExtraBtn ? "8.5rem" : "5.438rem"} 1.5rem;
+	margin: 0 auto;
+	background-color: ${theme.color.bg};
+	${theme.font.body_b};
+`;
 
-	chattingWrap: (isExtraBtn: boolean) => css`
-		padding: 5.75rem 1.5rem ${isExtraBtn ? "8.5rem" : "5.438rem"} 1.5rem;
-		margin: 0 auto;
-		${theme.font.body_b};
-	`,
-
-	progress: css`
-		position: fixed;
-		left: 50%;
-		transform: translate(-50%);
-		width: 22.5rem;
-		padding-top: 3.5rem;
-		appearance: none;
-		::-webkit-progress-bar {
-			background: white;
-			height: 4px;
-			overflow: hidden;
-		}
-		::-webkit-progress-value {
-			background: ${theme.color.main_blue};
-		}
-	`,
-};
+const progressStyle = css`
+	position: fixed;
+	left: 50%;
+	transform: translateX(-50%);
+	width: 22.5rem;
+	padding-top: 3.5rem;
+	appearance: none;
+	::-webkit-progress-bar {
+		background: white;
+		height: 4px;
+		overflow: hidden;
+	}
+	::-webkit-progress-value {
+		background: ${theme.color.main_blue};
+	}
+`;
