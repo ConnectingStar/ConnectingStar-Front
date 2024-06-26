b7f48e31a4767acdf8e0d645db808542a050cfcf
khkh0109 <ist200120@gmail.com>
2024-06-26 09:51:26 +0900

    [TS-346] Fix: axios 인스턴스 교체

--- src/api/star/starThunk.ts
+++ src/api/star/starThunk.ts
@@ -6,7 +6,7 @@ import { END_POINTS } from "@/constants/api";
 
 export const getStarMain = createAsyncThunk("star/getStarMain", async (_, thunkOptions) => {
 	try {
-		const { data } = await axiosInstance.get(END_POINTS.STAR_MAIN);
+		const { data } = await authorizedAxiosInstance.get(END_POINTS.STAR_MAIN);
 		return data;
 	} catch (error) {
 		throw thunkOptions.rejectWithValue(error);
