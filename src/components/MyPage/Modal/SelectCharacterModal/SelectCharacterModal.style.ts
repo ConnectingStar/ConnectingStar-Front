0f646e9dc47653bb5f75ad564e173be477b4efae
sikkzz <sling0623@gmail.com>
2024-07-08 18:40:33 +0900

    [TS-365] Refactor: select character modal refactoring

--- src/components/MyPage/Modal/SelectCharacterModal/SelectCharacterModal.style.ts
+++ src/components/MyPage/Modal/SelectCharacterModal/SelectCharacterModal.style.ts
@@ -17,18 +17,15 @@ export const layoutStyle = css`
 
 export const characterWrapperStyle = css`
 	margin: 0.6875rem 0 1rem;
-	display: flex;
-	flex-wrap: wrap;
-	justify-content: space-between;
-	row-gap: 0.375rem;
+	display: grid;
+	grid-template-columns: repeat(3, 1fr);
+	gap: 0.375rem;
 `;
 
-export const getCharacterBoxStyle = (isSelect: boolean) => {
-	return css`
-		width: 6.25rem;
-		height: 6.25rem;
-		border-radius: 15px;
-		background-color: #d9d9d9;
-		border: ${isSelect ? `5px solid ${theme.color.main_blue}` : "none"};
-	`;
-};
+export const characterBoxStyle = (isSelect: boolean) => css`
+	width: 6.25rem;
+	height: 6.25rem;
+	border-radius: 15px;
+	border: ${isSelect ? `5px solid ${theme.color.main_blue}` : "none"};
+	opacity: ${!isSelect ? "0.8" : ""};
+`;
