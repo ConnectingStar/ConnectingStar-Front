ab34915e36e87e14bbb9d308b9607a10af0413c3
sikkzz <sling0623@gmail.com>
2024-07-18 10:36:37 +0900

    [TS-353] Refactor: star prize modal refacotring

--- src/constants/homeConstants.tsx
+++ src/constants/homeConstants.tsx
@@ -65,26 +65,25 @@ export const DELETE_REASON_DATA = [
 	},
 ];
 
-export const prizeComments = [
+export const STAR_PRAIZE_TEXT_DATA = [
 	{
+		id: "less",
 		blueText: `완벽의 강박을 덜어낸\n당신에게 `,
 		yellowText: `별을 드릴게요!`,
-		comment: `[정체성한] 사람에\n한걸음 더 다가갔군요 😊`,
+		comment: ` 사람에\n한 걸음 더 다가갔군요 😊`,
 	},
 	{
+		id: "enough",
 		blueText: `생각한 대로 해내는\n당신에게 `,
 		yellowText: `별을 드려요!`,
-		comment: `정체성 [정체성한]이\n한층 뚜렷해졌어요 😊`,
+		comment: `이\n한층 뚜렷해졌어요 😊`,
 	},
 	{
+		id: "many",
 		blueText: `목표치를 초과 달성한\n당신에게 `,
 		yellowText: `별을 드립니다!`,
-		comment: `[정체성한] 사람에\n한걸음 더 다가갔군요 😊`,
+		comment: ` 사람에\n가까워지고 있어요 😊`,
 	},
-	// {
-	// 	blueText: `시작이 반!`,
-	// 	comment: `더욱 {정체성한} 사람이 되기 위한 한 걸음\n제가 {닉네임}님을 응원할게요 😊`,
-	// },
 ];
 
 export type AlarmData = {
