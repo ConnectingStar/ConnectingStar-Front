20107af2fa7eab5fd2a46f42bb394a05d6b5e785
sikkzz <sling0623@gmail.com>
2024-07-19 17:35:04 +0900

    [TS-353] Refactor: habit record disabled condition add

--- src/components/Habit/HabitRecord/HabitRecord.style.ts
+++ src/components/Habit/HabitRecord/HabitRecord.style.ts
@@ -7,7 +7,7 @@ export const layoutStyle = css`
 	flex-direction: column;
 	width: 22.5rem;
 	margin: 0 auto;
-	padding: 4.75rem 1.5rem 5.438rem;
+	padding: 4.75rem 1.5rem 1rem;
 	gap: 40px;
 	white-space: pre-wrap;
 	${theme.font.body_a};
@@ -81,7 +81,7 @@ export const contentInputStyle = (isChanged?: boolean) => css`
 	resize: none;
 	width: 100%;
 	outline: none;
-	color: ${isChanged ? theme.color.black : theme.color.button_deactivated};
+	color: ${isChanged === false && theme.color.button_deactivated};
 
 	&::placeholder {
 		${theme.color.button_deactivated}
