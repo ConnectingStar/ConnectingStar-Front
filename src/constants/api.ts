258d00b84092c84c9347b8f66eb3f885ea67573e
sikkzz <sling0623@gmail.com>
2024-07-29 09:23:03 +0900

    [TS-365] Refactor: edit profile image api refactoring

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -35,6 +35,7 @@ export const END_POINTS = {
 		`constellation/list?constellationTypeId=${id}&isRegistered=${isRegistered}`,
 	STAR_DETAIL: (id: string) => `constellation?constellationId=${id}`,
 	USER_CONSTELLATION: "/user/constellation",
+	USER_CONSTELLATION_V2: "/v2/users/me/constellation",
 	ADD_STAR: "user/constellation/star",
 };
 
