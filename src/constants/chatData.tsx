a85cec9be130d56f0a30926f6190ad040a182b1f
khkh0109 <ist200120@gmail.com>
2024-08-08 10:46:48 +0900

    [TS-384] Feat: chatData 문구, 스타일 수정

--- src/constants/chatData.tsx
+++ src/constants/chatData.tsx
@@ -1,7 +1,5 @@
 import { modalType } from "@/constants/modalConstants";
 
-import { theme } from "@/styles/theme";
-
 import type { HabitRequestType } from "@/types/habit";
 
 export function createChatData(habitRequest: HabitRequestType, nickname?: string) {
@@ -9,9 +7,9 @@ export function createChatData(habitRequest: HabitRequestType, nickname?: string
 		{
 			id: "firstMeet",
 			botMessage: [
-				`반가워요 ${nickname}님!\n저는 약속 맺기를 도울 타스예요 :)`,
-				`어떤 습관을 함께 만들어 볼까요?`,
-				`매일 해도 무리 없는 쉬운 것부터 시작하기를 추천해요 😊`,
+				`반갑습니다🙂 저는 ${nickname}님의 습관 설계를 도와드릴 타스라고 해요`,
+				`무엇을 우리의 첫 약속으로 시작해 볼까요?`,
+				`처음엔 매일 해도 무리 없는 쉬운 것부터 시작하는 게 좋아요😊`,
 			],
 			bottomButton: ["습관 선택"],
 			userMessage: `${habitRequest.behavior}`,
@@ -20,9 +18,9 @@ export function createChatData(habitRequest: HabitRequestType, nickname?: string
 		{
 			id: "behavior",
 			botMessage: [
-				`그렇군요!`,
-				`이번엔 정체성을 정해 볼게요`,
-				`${habitRequest.behavior}을(를) 통해서 ${nickname}님은 어떤 사람이 되고 싶으세요?`,
+				`좋아요, 이번엔 정체성을 정해 보죠`,
+				// TODO: 조사 을를 적용
+				`${habitRequest.behavior}를 통해 ${nickname}님은 어떤 사람이 되고 싶으세요?`,
 			],
 			bottomButton: ["정체성 선택"],
 			userMessage: `${habitRequest.identity}`,
@@ -31,31 +29,28 @@ export function createChatData(habitRequest: HabitRequestType, nickname?: string
 		{
 			id: "identity",
 			botMessage: [
-				`좋습니다 :)`,
-				`지금 이 순간부터 ${nickname}님은 ${habitRequest.identity} 사람이에요`,
-				`오늘부터 진심으로 ${nickname}님은 ${habitRequest.identity} 사람이라고 믿어 주세요!`,
+				`좋습니다, 지금 이 순간부터 ${nickname}님은 ${habitRequest.identity} 사람이라고 믿는 거예요💪`,
 			],
-			bottomButton: ["그런데 정체성을 왜 정하는 거야?"],
-			userMessage: "그런데 정체성을 왜 정하는 거야?",
+			bottomButton: ["그런데 정체성은 갑자기 왜?"],
+			userMessage: "그런데 정체성은 갑자기 왜?",
 		},
 		{
 			id: "reasonIdentity",
 			botMessage: [
-				`정체성을 정함으로써 스스로에게 믿음을 부여할 수 있어요`,
-				`그리고 우리는 놀랍도록 스스로가 믿는 대로 행동하죠`,
+				`우리는 놀라울 정도로 스스로가 믿는대로 행동하고 또 그렇게 변화하기 때문이에요`,
+				`처음엔 조금 어색하겠지만, 믿음이 쌓여가는 하루가 늘어갈 수록 어느새 믿는 대로 변화한 ${nickname}님의 모습에 놀라게 될 거예요🙂️`,
 			],
-			bottomButton: ["알겠어!"],
-			userMessage: "알겠어!",
+			bottomButton: ["좋아,  믿어볼게💪"],
+			userMessage: "좋아,  믿어볼게💪",
 		},
 		{
 			id: "runTime",
 			botMessage: [
-				`이제 본격적으로 습관을 구체화 시켜볼까요?`,
-				`습관 약속은 시간, 장소, 행동으로 구분돼요`,
-				`그리고 그 약속은\n1. 무엇을 할지 <span style="color: ${theme.color.main_blue};font-weight: 700;">명확</span>해야 하고\n2. 하고 싶도록 <span style="color: ${theme.color.main_blue};font-weight: 700;">매력</span>적이며\n3. <span style="color: ${theme.color.main_blue};font-weight: 700;">쉽게</span> 할 수 있어야 하고\n4. 하고 난 뒤 <span style="color: ${theme.color.main_blue};font-weight: 700;">만족</span>스러워야 합니다`,
-				`최대한 4가지 원칙에 맞게끔 습관을 만들어 볼까요?`,
-				`먼저 시간을 정해 볼게요!`,
-				`다른 일에 방해 받지 않는 시간 혹은 매일 지키기에 수월한 시간으로 설정해 주세요`,
+				`지금부터 습관 약속을 본격적으로 구체화 시켜볼게요`,
+				`먼저, 약속은 시간/장소/행동 세 가지 요소로 구분됩니다`,
+				`그리고 아래의 네 규칙을 지킬 수록 강력한 변화를 이끌어 낼 거예요`,
+				`1. <span class="bold blue">명확</span>할 것\n2. <span class="bold blue">매력</span>적일 것\n3. <span class="bold blue">쉬울</span> 것\n4. <span class="bold blue">만족</span>스러울 것`,
+				`시간부터 정해 볼게요⏰ 매일 지키기 쉽고 다른 일에 방해 받지 않는 시간은 언제인가요?`,
 			],
 			bottomButton: ["시간 선택"],
 			userMessage: `${habitRequest.runTime.noon} ${habitRequest.runTime.hour}:${habitRequest.runTime.minute}`,
@@ -63,10 +58,9 @@ export function createChatData(habitRequest: HabitRequestType, nickname?: string
 		},
 		{
 			id: "place",
+			// TODO: 조사 을를 적용
 			botMessage: [
-				`다음으로 장소를 정해 볼게요!`,
-				`어떤 장소가 ${nickname}님에게 매력적인가요?`,
-				`가는 것만으로 만족스럽거나 습관을 쉽게 할 수 있는 곳으로 명확하게 적어 주세요`,
+				`이번엔 장소를 정해 볼게요🧭 ${habitRequest.behavior}를 쉽게 할 수 있거나 가는 것만으로 기분이 좋아지는 곳이 있나요?`,
 			],
 			bottomButton: ["장소 선택"],
 			userMessage: `${habitRequest.place}`,
@@ -76,10 +70,9 @@ export function createChatData(habitRequest: HabitRequestType, nickname?: string
 		{
 			id: "behaviorUnit",
 			botMessage: [
-				`마지막으로 행동을 정해 볼게요`,
-				`난이도는 매일 할 수 있을 정도로 쉽게 시작해야 해요!`,
-				`예를들어 책을 읽는다면 30분보다 3장 처럼요 :D`,
-				`3장으로 시작했지만, 점차 자리 잡으면 30장도 쉬운 일이 되어 있을 거예요!`,
+				// TODO: 조사 을를 적용
+				`다음으로 ${habitRequest.behavior}를 얼마나 할지 정해 보겠습니다🚩 이때, 가능한 쉬운 수준으로 시작해 보세요`,
+				`그러다 일주일 내내 실천할 수 있게 될 때쯤, 난이도를 살짝 높이고 주기적으로 수준을 높여가는 거예요😉`,
 			],
 			bottomButton: ["실천 정도 선택"],
 			userMessage: `${habitRequest.behaviorValue} ${habitRequest.behaviorUnit}`,
@@ -87,7 +80,10 @@ export function createChatData(habitRequest: HabitRequestType, nickname?: string
 		},
 		{
 			id: "alert",
-			botMessage: [`약속을 기억하고 실천을 기록하실 수 있도록 하루 두 번, 알림을 보내드릴게요!`],
+			botMessage: [
+				`좋습니다! 마지막으로 알림 받을 시간을 선택해 주세요☺️`,
+				`약속을 기억하고 실천을 기록할 수 있게 하루 두 번, 알림을 보내드립니다`,
+			],
 			bottomButton: ["이대로 진행", "1차 알림 변경", "2차 알림 변경"],
 			userMessage: `
 			<div class="alert">
@@ -107,7 +103,9 @@ export function createChatData(habitRequest: HabitRequestType, nickname?: string
 		},
 		{
 			id: "organize",
-			botMessage: [`지금까지 나눈 이야기를 정리해 보여드릴게요`],
+			botMessage: [
+				`이제 거의 다 왔어요! 지금까지 나눈 이야기들을  정리해 보여드릴게요😊 수정할 부분이 있나요?`,
+			],
 			bottomButton: [
 				"이 내용이 맞아",
 				"정체성 변경",
@@ -176,23 +174,21 @@ export function createChatData(habitRequest: HabitRequestType, nickname?: string
 		{
 			id: "inform",
 			botMessage: [
-				`습관 실천이 가장 핵심이지만, 기록 역시 정말 중요해요 :)`,
-				`무엇 때문에 쉬었는지 혹은 오늘 실천은 어땠는지 매일 제게 알려주세요!`,
+				`마지막으로 습관 형성에 중요한 <span class="bold blue">두 가지 주의 사항</span>을 말씀드릴게요`,
+				`<span class="bold blue">첫째,</span> 시간/장소/정도에 너무 얽매이지 마세요🙅 물론 지키면 좋지만, 중요한 건 <span class="bold blue">일단 하는 거</span>니까요`,
+				`<span class="bold blue">둘째,</span> 실천했든 쉬었든 그날의 기록을 남겨 주세요📝 <span class="bold blue">기록은 그 자체로 강력한 동기부여 수단</span>이 된답니다`,
 			],
-			bottomButton: ["알겠어!"],
-			userMessage: "알겠어!",
+			bottomButton: ["기억할게, 일단 하기랑 기록 남기기 맞지?"],
+			userMessage: "기억할게, 일단 하기랑 기록 남기기 맞지?",
 		},
 		{
 			id: "last",
 			botMessage: [
-				`감사합니다. 실천을 기록하신 날에는 별을 드릴게요🌟`,
-				`모은 별로  별자리를 완성시킬 때마다 캐릭터를 획득할 수 있어요 :)`,
-				`성운 마을의 귀여운 친구들이 ${nickname}님과 만날 날을 기다리고 있답니다 :D`,
-				`Tars와 맺는 습관 약속은 <span style="color: ${theme.color.main_blue};font-weight: 700;">매일 실행</span>하는 것을 권장하고 있어요`,
-				`매번 완벽할 필요 없습니다`,
-				`약속의 단 10%만 지켜지더라도 \n<span style="color: ${theme.color.main_blue};font-weight: 700;">"꾸준하게 하는 것”</span>이 핵심이니까요`,
-				`고생 많으셨습니다!`,
-				`${habitRequest.behavior}를 꾸준히 하는 ${nickname}님을 응원할게요 XD`,
+				`네 맞아요, 감사합니다😊 그리고 그거 아세요?`,
+				`저 뿐만 아니라 다른 친구들도 ${nickname}님을 보고싶어 한다는 사실! `,
+				`하지만 부끄러움이 많아 별자리 뒤에 숨어 있답니다🫣`,
+				`<span class="bold">실천 기록으로 받은 별</span>을 별자리에 하나 둘 넣다 보면 어느새 친구들이 가득하게 될 거예요😉`,
+				`<span class="bold blue">스스로를 믿으며 꾸준히 나아갈</span> ${nickname}님을 응원하겠습니다💪 앞으로 잘 부탁드려요!`,
 			],
 			bottomButton: ["나도 잘 부탁해!"],
 			userMessage: "",
