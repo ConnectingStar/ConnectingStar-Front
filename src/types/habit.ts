a48838e867126afa87185e653156dff4ed827fab
Woojin Lee <dev.woojin.lee@gmail.com>
2024-08-09 22:58:26 +0900

    feature: (habit) add post request v2

--- src/types/habit.ts
+++ src/types/habit.ts
@@ -7,6 +7,10 @@ export interface HabitDeleteRequestType {
 	reason: string;
 }
 
+/**
+ * 프론트에서 공용으로 사용하는 Habit 객체 타입.
+ * /habit API v1의 요청 타입과 동일
+ */
 export interface HabitRequestType {
 	runHabitId?: number;
 	identity: string;
@@ -17,6 +21,61 @@ export interface HabitRequestType {
 	behaviorUnit: string;
 	firstAlert: { noon: string; hour: string; minute: string };
 	secondAlert: { noon: string; hour: string; minute: string };
+}
+
+/**
+ * 백엔드 v2 POST API의 Request Body Type.
+ * `POST /v2/habits` 의 Request Body Type을 그대로 사용함.
+ */
+export interface HabitPostV2RequestType {
+	 /**
+     * 정체성
+     */
+	 identity: string;
+ 
+	 /**
+	  * 실천 시간.
+	  * @type {String} "HH:mm"
+	  */
+	 runTime: string;
+ 
+	 /**
+	  * 장소
+	  */
+	  place: string;
+ 
+	 /**
+	  * 행동
+	  */
+	  action: string;
+ 
+	 /**
+	  * 얼마나
+	  */
+	  value: number;
+ 
+	 /**
+	  * 단위
+	  */
+	  unit: string;
+ 
+	 /**
+	  * 1차 알림 (값이 없을 시 자동으로 runTime 10분 전으로 설정)
+	  * @type {String} "HH:mm"
+	  */
+	  firstAlert?: string;
+ 
+	 /**
+	  * 2차 알림 (값이 없을 시 자동으로 runTime 30분 후로 설정)
+	  * @type {String} "HH:mm"
+	  */
+	  secondAlert?: string;
+ 
+	 /**
+	  * 온보딩 페이지에서 호출했으면 true.
+	  * 유저의 온보딩 정보를 업데이트한다.
+	  */
+	  isOnboarding?: boolean;
 }
 
 export interface HabitRestRecordRequestType {
